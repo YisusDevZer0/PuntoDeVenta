49b810d01f27079a7d3b563abf255268b9061765
SaludaDev <jesusemutul@gmail.com>
2024-01-07 20:06:23 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
 	<link rel="stylesheet" href="css/main.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	
 	<script src="js/material.min.js" ></script>
 	<script src="js/sweetalert2.min.js" ></script>
 	<script src="js/jquery.mCustomScrollbar.concat.min.js" ></script>
