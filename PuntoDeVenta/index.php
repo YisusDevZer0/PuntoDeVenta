b9c4c398dc4d671752088112ccb42703abfae391
YisusZero <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-16 21:48:25 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -1,11 +1,12 @@
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>PUNTO DE VENTA</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PUNTO DE VENTA</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" type="text/css" href="Componentes/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="Componentes/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="Componentes/vendor/animate/animate.css">
 <!--===============================================================================================-->	
@@ -32,109 +33,149 @@
 <script type="text/javascript" src="Consultas/validation.min.js"></script>
 <script type="text/javascript" src="Consultas/POS3.js"></script>
 
-    <style>
-        .error {
-            color: red;
-            margin-left: 5px;
-        }
-    </style>
 
 </head>
 <body style="background-color: #2FDDEE;">
+   <style>
+        .error {
+  color: red;
+  margin-left: 5px; 
+  
+}
 
-    <!-- Navbar -->
-    <!-- Your existing navbar code -->
-
-    <div class="container-login100">
-        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-            <form class="login100-form validate-form" method="post" id="login-form" autocomplete="off">
-
-                <span class="login100-form-title p-b-49">
-                    <?php echo $mensaje ?>
-                </span>
-
-                <div class="wrap-input100">
-                    <span class="label-input100">Correo electrónico</span>
-                    <input class="input100" type="email" autocomplete="off" required placeholder="puntoventa@consulta.com" name="user_email" id="user_email" maxlength="50">
-                    <span class="focus-input100" data-symbol="&#xf206;"></span>
-                </div>
-
-                <div class="wrap-input100">
-                    <span class="label-input100">Contraseña</span>
-                    <input class="input100" type="password" required placeholder="************" autocomplete="new-password" name="password" id="password">
-                    <span class="focus-input100" data-symbol="&#xf190;"></span>
-                </div>
-
-                <br>
-
-                <div class="checkbox">
-                    <label>
-                        <input id="show_password" type="checkbox"> Mostrar contraseña
-                    </label>
-                </div>
-
-                <div class="container-login100-form-btn">
-                    <div class="wrap-login100-form-btn">
-                        <div class="login100-form-bgbtn"></div>
-                        <button class="login100-form-btn" type="submit" name="login_button" id="login_button" style="background-color: #C80096;">
-                            Ingresar
-                        </button>
-                    </div>
-                </div>
-
-            </form>
-            <div id="error" class="error"></div>
-        </div>
+    </style>
+<div class="fish-container">
+        <div class="fish"></div>
+        <div class="fish"></div>
+        <div class="fish"></div>
     </div>
+
+
+
+
+<!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
+  <a class="navbar-brand" href="#">PUNTO DE VENTA  <i  class="fas fa-receipt fa-2x fa-lgfa-2x fa-lg"></i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+    <ul class="navbar-nav mr-auto">
+   
+    </ul>
+    
+  </div>
+</nav>
+<!--/.Navbar -->
+
+
+        
+		<div class="container-login100" >
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+    
+				<form class="login100-form validate-form" method="post" id="login-form" autocomplete="off">
+
+					<span class="login100-form-title p-b-49">
+						<?echo $mensaje?>
+					</span>
+
+					<div class="wrap-input100 " >
+						<span class="label-input100">Correo electronico</span>
+						<input class="input100" input type="email" autocomplete="off" required placeholder="puntoventa@consulta.com" name="user_email" id="user_email" maxlength="50">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
+
+					<div class="wrap-input100 ">
+						<span class="label-input100">Contraseña</span>
+						<input class="input100" type="password" required placeholder="************" autocomplete="new-password" name="password" id="password"  maxlength="10">
+                       
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+                        
+                    </div>
+                    <br>
+                    <div class="checkbox">
+    <label>
+    <input id="show_password" type="checkbox" /> Mostrar contraseña
+    </label>
+  </div>   
+ 
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" type="submit"  name="login_button" id="login_button"  style="background-color: #C80096;">
+								Ingresar
+							</button>
+						</div>
+					</div>
+                 
+                    </form>  <div id="error">
+  </div>
+ 
+
+					
+					<!--Start of Tawk.to Script-->
+
+<!--End of Tawk.to Script-->
+			
+			</div>
+		</div>
+	</div>
+	
+<!-- Modal hacia soporte -->
+
+    
     <footer class="page-footer font-small default-color">
 
-<!-- Copyright -->
+  <!-- Copyright -->
 
-<b>PUNTO DE VENTA</b> | Version 1.0
-</div>
-<!-- Copyright -->
+  <b>PUNTO DE VENTA</b> | Version 1.0
+  </div>
+  <!-- Copyright -->
 
 </footer>
 <!-- Footer -->
 
 
 <!--===============================================================================================-->
-
-<script src="Componentes/vendor/animsition/js/animsition.min.js"></script>
+	
+	<script src="Componentes/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-<script src="Componentes/vendor/bootstrap/js/popper.js"></script>
-<script src="Componentes/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="Componentes/vendor/bootstrap/js/popper.js"></script>
+	<script src="Componentes/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-<script src="Componentes/vendor/select2/select2.min.js"></script>
+	<script src="Componentes/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-<script src="Componentes/vendor/daterangepicker/moment.min.js"></script>
-<script src="Componentes/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="Componentes/vendor/daterangepicker/moment.min.js"></script>
+	<script src="Componentes/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-<script src="Componentes/vendor/countdowntime/countdowntime.js"></script>
+	<script src="Componentes/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-<script src="Componentes/js/main.js"></script>
-    <!-- Your existing footer code -->
-
-    <!-- Your existing scripts -->
-
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-    <script type="text/javascript">
-        // When the checkbox changes state.
-        $('#show_password').on('change', function (event) {
-            // If the checkbox is checked
-            if ($('#show_password').is(':checked')) {
-                // Convert the password input to text.
-                $('#password').get(0).type = 'text';
-            // Otherwise...
-            } else {
-                // Convert it back to a password.
-                $('#password').get(0).type = 'password';
-            }
-        });
-
-        $('#login-form').attr('autocomplete', 'off');
-    </script>
+	<script src="Componentes/js/main.js"></script>
 
 </body>
 </html>
+
+    <script type="text/javascript">
+$(window).load(function() {
+    $(".loader").fadeOut(1000);
+});
+</script>
+
+<script>
+
+   // Cuando el checkbox cambie de estado.
+$('#show_password').on('change',function(event){
+   // Si el checkbox esta "checkeado"
+   if($('#show_password').is(':checked')){
+      // Convertimos el input de contraseña a texto.
+      $('#password').get(0).type='text';
+   // En caso contrario..
+   } else {
+      // Lo convertimos a contraseña.
+      $('#password').get(0).type='password';
+   }
+});
+
+$('#login-form').attr('autocomplete', 'off');
+</script>
