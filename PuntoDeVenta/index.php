4eb700a5e8887624ef0c93da5450b3405678f301
YisusZero <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-16 21:48:07 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -86,7 +86,33 @@
             <div id="error" class="error"></div>
         </div>
     </div>
+    <footer class="page-footer font-small default-color">
 
+<!-- Copyright -->
+
+<b>PUNTO DE VENTA</b> | Version 1.0
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+<!--===============================================================================================-->
+
+<script src="Componentes/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="Componentes/vendor/bootstrap/js/popper.js"></script>
+<script src="Componentes/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="Componentes/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="Componentes/vendor/daterangepicker/moment.min.js"></script>
+<script src="Componentes/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="Componentes/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="Componentes/js/main.js"></script>
     <!-- Your existing footer code -->
 
     <!-- Your existing scripts -->
