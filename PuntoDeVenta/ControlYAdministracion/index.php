23a6ca9ce570e68ed4a9b77634e18543678de9e7
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-17 22:46:17 -0600

    img

--- PuntoDeVenta/ControlYAdministracion/index.php
+++ PuntoDeVenta/ControlYAdministracion/index.php
@@ -129,7 +129,7 @@ include_once "Controladores/ControladorUsuario.php"
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <img class="rounded-circle me-lg-2" src="https://doctorpez.mx/PuntoDeVenta/PerfilesImg/<?php echo $row['file_name']?>" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">John Doe</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
