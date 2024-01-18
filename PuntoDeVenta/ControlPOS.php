c7bdd06886b784ff8f33bee624add2d839562584
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-17 21:25:42 -0600

    Datos_Administrativo

--- PuntoDeVenta/ControlPOS.php
+++ PuntoDeVenta/ControlPOS.php
@@ -6,7 +6,7 @@ if($_SESSION["SuperAdmin"])	//Condicion admin
 {
 	
 
-	header("location:https://doctorpez.mx/ControlYadministracion");	
+	header("location:https://doctorpez.mx/PuntoDeVenta/ControlYadministracion");	
 
 }
 if($_SESSION["VentasPos"])	//Condicion personal
