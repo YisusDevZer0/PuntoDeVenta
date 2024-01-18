8268cae602141ea2cce1a562fdf71bf2799b9f46
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-17 22:15:44 -0600

    ajustesencodigoindex

---
+++ PuntoDeVenta/ControlYAdministracion/Controladores/ControladorUsuario.php
@@ -0,0 +1,53 @@
+<?php 
+date_default_timezone_set("America/Monterrey");
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+
+if(!isset($_SESSION['SuperAdmin'])){
+	header("Location: Expiro.php");
+}
+include_once("db_connection.php");
+$sql = "SELECT
+Usuarios_PV.Id_PvUser,
+Usuarios_PV.Nombre_Apellidos,
+Usuarios_PV.file_name,
+Usuarios_PV.Fk_Usuario,
+Usuarios_PV.Fecha_Nacimiento,
+Usuarios_PV.Correo_Electronico,
+Usuarios_PV.Telefono,
+Usuarios_PV.AgregadoPor,
+Usuarios_PV.AgregadoEl,
+Usuarios_PV.Estatus,
+Usuarios_PV.Licencia,
+Tipos_Usuarios.ID_User,
+Tipos_Usuarios.TipoUsuario,
+Sucursales.ID_Sucursal,
+Sucursales.Nombre_Sucursal
+FROM
+Usuarios_PV
+INNER JOIN Tipos_Usuarios ON Usuarios_PV.Fk_Usuario = Tipos_Usuarios.ID_User
+INNER JOIN Sucursales ON Usuarios_PV.Fk_Sucursal = Sucursales.ID_Sucursal;
+Usuarios_PV.Id_PvUser='".$_SESSION['ControlMaestro']."'";
+$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+$row = mysqli_fetch_assoc($resultset);
+$hora = date('G'); if (($hora >= 0) AND ($hora < 6)) 
+  { 
+    
+    $mensaje = "Hola, que tengas una excelente madrugada."; 
+  } 
+  else if (($hora >= 6) AND ($hora < 12)) 
+  { 
+    $mensaje = "Buenos días"; 
+  } 
+  else if (($hora >= 12) AND ($hora < 18)) 
+  { 
+    $mensaje = "Buenas tardes"; 
+  } 
+  else
+  { 
+  $mensaje = "Buenas noches"; 
+  } 
+
+   ?>
+
