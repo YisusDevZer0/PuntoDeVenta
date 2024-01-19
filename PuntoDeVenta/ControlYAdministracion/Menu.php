7a4f8a06349d91d14692bb8828d9912c0c47ff8d
SaludaDev <jesusemutul@gmail.com>
2024-01-18 20:58:19 -0600

    Update Menu.php

--- PuntoDeVenta/ControlYAdministracion/Menu.php
+++ PuntoDeVenta/ControlYAdministracion/Menu.php
@@ -16,11 +16,11 @@
                 <div class="navbar-nav w-100">
                     <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Inicio</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-users-gear"></i></i>Personal</a>
                         <div class="dropdown-menu bg-transparent border-0">
-                            <a href="button.html" class="dropdown-item">Buttons</a>
-                            <a href="typography.html" class="dropdown-item">Typography</a>
-                            <a href="element.html" class="dropdown-item">Other Elements</a>
+                            <a href="button.html" class="dropdown-item"><i class="fa-solid fa-users"></i>Tipos de usuarios</a>
+                            <a href="typography.html" class="dropdown-item">Personal activo </a>
+                            <a href="element.html" class="dropdown-item">Personal inactivo</a>
                         </div>
                     </div>
 
