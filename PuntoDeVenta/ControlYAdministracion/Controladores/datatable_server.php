8021262cc8a8c68c25ec184ac3d70826c6f10e9d
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-19 21:18:51 -0600

    Update datatable_server.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/datatable_server.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/datatable_server.php
@@ -13,15 +13,13 @@ $columns = array(
     array('db' => 'TipoUsuario', 'dt' => 1),
     array('db' => 'Licencia', 'dt' => 2),
     array('db' => 'Creadoel', 'dt' => 3),
-    array('db' => 'Creado', 'dt' => 4),
-    array('db' => '', 'dt' => 5)  // Esta columna está vacía porque no proviene de la base de datos, es para el botón 'Editar'
+    array('db' => 'Creado', 'dt' => 4)
+    
 );
 
 // Utiliza la función de SSP para obtener los datos
 $result = SSP::simple($_GET, $dbDetails, $table, $primaryKey, $columns);
 
-// Después de obtener los datos usando SSP
-var_dump($result);
+// Convierte el resultado a un formato que DataTables pueda entender
 echo json_encode($result);
-
 ?>
