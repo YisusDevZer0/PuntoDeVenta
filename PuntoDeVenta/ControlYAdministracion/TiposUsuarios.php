8946626139b44b8910cc6a19ba3b7d38a6368e23
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-19 20:46:51 -0600

    Update TiposUsuarios.php

--- PuntoDeVenta/ControlYAdministracion/TiposUsuarios.php
+++ PuntoDeVenta/ControlYAdministracion/TiposUsuarios.php
@@ -62,7 +62,7 @@ $(document).ready(function () {
     var table = $('#userTable').DataTable({
         "processing": true,
         "serverSide": true,
-        "ajax": "datatable_server.php",
+        "ajax": "Controladores/datatable_server.php",
         "columns": [
             { "data": "ID_User" },
             { "data": "TipoUsuario" },
