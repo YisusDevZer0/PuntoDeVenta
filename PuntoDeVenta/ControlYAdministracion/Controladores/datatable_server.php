2b10729f622d586687dc9fe0f846ba4518de5500
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-19 21:24:30 -0600

    Update datatable_server.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/datatable_server.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/datatable_server.php
@@ -13,8 +13,8 @@ $columns = array(
     array('db' => 'TipoUsuario', 'dt' => 1),
     array('db' => 'Licencia', 'dt' => 2),
     array('db' => 'Creadoel', 'dt' => 3),
-    array('db' => 'Creado', 'dt' => 4)
-    
+    array('db' => 'Creado', 'dt' => 4),
+    array('db' => '', 'dt' => 5)
 );
 
 // Utiliza la función de SSP para obtener los datos
