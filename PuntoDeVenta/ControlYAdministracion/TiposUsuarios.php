fbfc7ce0001cf166ca53ecb4e1f293ffe3ab553b
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-19 20:42:05 -0600

    Update TiposUsuarios.php

--- PuntoDeVenta/ControlYAdministracion/TiposUsuarios.php
+++ PuntoDeVenta/ControlYAdministracion/TiposUsuarios.php
@@ -154,7 +154,7 @@ $(document).ready(function () {
     </div>
 
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/chart/chart.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
