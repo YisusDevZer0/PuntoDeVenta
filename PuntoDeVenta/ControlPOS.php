b7c4c632dfda920a08e17a4dcb37f0f37ad8af1b
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-20 18:39:30 -0600

    Update ControlPOS.php

--- PuntoDeVenta/ControlPOS.php
+++ PuntoDeVenta/ControlPOS.php
@@ -6,7 +6,7 @@ if($_SESSION["ControlMaestro"])	//Condicion admin
 {
 	
 
-	header("location:https://doctorpez.mx/PuntoDeVenta/ControlYadministracion/");	
+	header("location:https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/");	
 
 }
 
