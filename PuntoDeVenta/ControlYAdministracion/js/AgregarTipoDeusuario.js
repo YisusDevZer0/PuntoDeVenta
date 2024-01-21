8b97eb5868d5695c6a08edffb439fea7672afac4
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-01-20 18:47:49 -0600

    xd

--- PuntoDeVenta/ControlYAdministracion/js/AgregarTipoDeusuario.js
+++ PuntoDeVenta/ControlYAdministracion/js/AgregarTipoDeusuario.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         // Realizar la solicitud Ajax
         $.ajax({
             type: 'POST',
-            url: 'https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/GuardarNuevoUsuario.php',
+            url: 'https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/NuevoTipoDeusuario.php',
             data: formData,
             processData: false,
             contentType: false,
