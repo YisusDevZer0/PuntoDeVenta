f5c9e1783092dde8658430f7a23e1182c78296fc
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-02-10 14:56:15 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -1,163 +1,102 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PUNTO DE VENTA</title>
-    <!-- Agrega los estilos de Material Design -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <style>
-        body {
-            background-color: #f0f0f0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .card {
-            width: 400px;
-            padding: 20px;
-        }
-        .card-content {
-            padding: 20px;
-        }
-        .card-action {
-            background-color: #C80096;
-            text-align: center;
-        }
-        .btn {
-            background-color: #C80096;
-        }
-        .btn:hover {
-            background-color: #960056;
-        }
-    </style>
+     <title>Animación de peces</title>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js"></script>
 </head>
+
 <body>
-    <div class="card">
-        <div class="card-content">
-            <span class="card-title">Inicio de sesión</span>
-            <!-- Agregar un div para mostrar el mensaje -->
-            <div id="mensaje" class="center-align"></div>
-            <div class="input-field">
-                <input id="user_email" type="email" class="validate">
-                <label for="user_email">Correo electrónico</label>
-            </div>
-            <div class="input-field">
-                <input id="password" type="password" class="validate">
-                <label for="password">Contraseña</label>
-            </div>
-        </div>
-        <div class="card-action">
-    <a href="#" id="btn_ingresar" class="btn waves-effect waves-light red">Ingresar</a>
-</div>
+     <script>
+          function preload() {
+               img = {
+                    pez: loadImage("https://i.ibb.co/phvXBP8/fish-unscreen-1.gif"),
+                    agua: loadImage("https://i.ibb.co/NTvPHG4/CK2ea0R.gif"),
+                    loto: loadImage("https://i.ibb.co/0CrhhhV/pixlr-bg-result.png"),
+               }
+          }
 
-    </div>
-    
-    <!-- Agrega los scripts de Materialize y el script PHP -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <?php
-    // Configuración de saludos según la hora del día
-    $saludos = array(
-        "Buenos días",
-        "Buenas tardes",
-        "Buenas noches"
-    );
+          peces = []
 
-    // Obtener la hora actual del servidor
-    $hora_actual = date("H");
+          function setup() {
+               createCanvas(800, 500)
+               img.agua.actualFrame = 0
+               for (let i = 0; i < 10; i++) {
+                    peces.push(new Pez())
+               }
+          }
 
-    // Determinar el saludo según la hora del día
-    if ($hora_actual >= 5 && $hora_actual < 12) {
-        $saludo = $saludos[0]; // Mañana
-    } elseif ($hora_actual >= 12 && $hora_actual < 18) {
-        $saludo = $saludos[1]; // Tarde
-    } else {
-        $saludo = $saludos[2]; // Noche
-    }
+          function draw() {
+               background("white")
+               dibujarPeces()
+               dibujarAgua()
+               dibujarLoto()
+               fullscreen();
+          }
 
-    // Configuración de mensajes de éxito y ventas en español e inglés
-    $mensajes_exito_ventas_es = array(
-        "¡Bienvenido de nuevo! 🚀 Prepárate para alcanzar nuevas alturas de éxito.",
-        "Te damos la bienvenida. El éxito y las ventas te esperan en cada paso que tomes. 💼",
-        "Hoy es otro día para lograr grandes ventas. ¡Vamos por ello! 💪",
-        "¡Hola campeón! Este es tu momento para brillar y cerrar esas ventas. 🌟",
-        "Bienvenido de vuelta. Estamos emocionados por tus éxitos venideros y ventas increíbles. 🎉",
-        "¡Cada nuevo día es una oportunidad para superarte a ti mismo! 🌈",
-        "La única forma de hacer un gran trabajo es amar lo que haces. 💙",
-        "¡Nunca subestimes el poder de la persistencia y la dedicación! 🚀",
-        "El éxito es la suma de pequeños esfuerzos repetidos día tras día. 💪",
-        "No hay atajos para el éxito, pero cada pequeño paso cuenta. 🏃‍♂️",
-        "La clave del éxito está en actuar con determinación y confianza. 🗝️",
-        "¡Tú eres capaz de lograr cosas asombrosas! Cree en ti mismo. 🌟",
-        "Cada desafío es una oportunidad para crecer. ¡Acepta el desafío! 🌱",
-        "La perseverancia no es una carrera larga; es muchas carreras cortas, una tras otra. 🏁",
-        "No te preocupes por los errores, son oportunidades para aprender y mejorar. 🛠️",
-        "El éxito es la suma de pequeños esfuerzos repetidos día tras día. 💼",
-        "Nunca es tarde para ser quien podrías haber sido. 🌅",
-        "La diferencia entre un sueño y un objetivo es un plan y un plazo. 🎯",
-        "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. 😊",
-    );
-    
-    $mensajes_exito_ventas_en = array(
-        "Welcome back! 🚀 Get ready to reach new heights of success.",
-        "Welcome aboard. Success and sales await you with every step you take. 💼",
-        "Today is another day to achieve great sales. Let's go for it! 💪",
-        "Hello champion! This is your time to shine and close those sales. 🌟",
-        "Welcome back. We are excited for your upcoming successes and incredible sales. 🎉",
-        "Every new day is an opportunity to surpass yourself! 🌈",
-        "The only way to do great work is to love what you do. 💙",
-        "Never underestimate the power of persistence and dedication! 🚀",
-        "Success is the sum of small efforts repeated day in and day out. 💪",
-        "There are no shortcuts to success, but every small step counts. 🏃‍♂️",
-        "The key to success is to act with determination and confidence. 🗝️",
-        "You are capable of achieving amazing things! Believe in yourself. 🌟",
-        "Every challenge is an opportunity to grow. Accept the challenge! 🌱",
-        "Perseverance is not a long race; it is many short races, one after another. 🏁",
-        "Don't worry about mistakes, they are opportunities to learn and improve. 🛠️",
-        "Success is the sum of small efforts repeated day in and day out. 💼",
-        "It's never too late to be who you could have been. 🌅",
-        "The difference between a dream and a goal is a plan and a deadline. 🎯",
-        "Success is not the key to happiness. Happiness is the key to success. 😊",
-    );
-    
-    // Función para obtener el mensaje en el idioma seleccionado
-    function obtener_mensaje_localizado($idioma) {
-        global $mensajes_exito_ventas_es, $mensajes_exito_ventas_en;
-        switch ($idioma) {
-            case 'es':
-                return $mensajes_exito_ventas_es[array_rand($mensajes_exito_ventas_es)];
-                break;
-            case 'en':
-                return $mensajes_exito_ventas_en[array_rand($mensajes_exito_ventas_en)];
-                break;
-            // Otros casos para más idiomas...
-            default:
-                return "Idioma no compatible";
-        }
-    }
+          function dibujarPeces() {
+               for (const p of peces) {
+                    p.dibujar()
+               }
+          }
 
-    // Obtener el mensaje en el idioma seleccionado (por ejemplo, español 'es' o inglés 'en')
-    $idioma_seleccionado = "es"; // Puedes obtener este valor dinámicamente según la configuración del usuario
-    $mensaje_localizado = obtener_mensaje_localizado($idioma_seleccionado);
-    ?>
+          class Pez {
+               constructor() {
+                    let angulo_inicio = random(2 * PI)
+                    this.posicion = p5.Vector.fromAngle(angulo_inicio, random(100, 300))
+                    this.posicion.add(width / 2, height / 2)
+                    this.aceleracion = createVector(
+                         -sin(angulo_inicio), cos(angulo_inicio)
+                    ).mult((random() < 0.5 ? -1 : 1) * random(5, 10))
+                    this.escala = random(0.2, 0.5)
+                    this.hue = random(0, 360)
+               }
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Muestra el mensaje de saludo dinámico
-            // M.toast({html: '<?php echo $saludo; ?>', classes: 'rounded'});
-            // Muestra el mensaje de éxito dinámico en el formulario
-            var mensajeDiv = document.getElementById('mensaje');
-            mensajeDiv.innerHTML = '<?php echo $mensaje_localizado; ?>';
-            
-            // Agrega un evento al botón de ingresar para mostrar el mensaje
-            document.getElementById('btn_ingresar').addEventListener('click', function() {
-                M.toast({html: 'Ingresando...', classes: 'rounded'});
-                // Aquí puedes agregar la lógica para enviar el formulario o realizar otras acciones
-            });
-        });
-    </script>
+               dibujar() {
+                    push()
+                    this.posicion.add(this.aceleracion.copy().mult(0.2))
+                    this.aceleracion.add(
+                         p5.Vector.fromAngle(
+                              atan2(
+                                   this.posicion.y - height / 2,
+                                   this.posicion.x - width / 2
+                              ),
+                              -0.1
+                         )
+                    )
+                    applyMatrix(
+                         this.escala, 0,
+                         0, this.escala,
+                         this.posicion.x, this.posicion.y
+                    )
+                    rotate(this.aceleracion.heading() - PI / 2)
+                    drawingContext.filter = `hue-rotate(${this.hue}deg)`
+                    image(img.pez, 0, 0)
+                    pop()
+               }
+          }
+
+          function dibujarAgua() {
+               push()
+               drawingContext.globalAlpha = 0.2
+               img.agua.setFrame(int(img.agua.actualFrame))
+               img.agua.actualFrame += 0.2
+               img.agua.actualFrame %= img.agua.numFrames()
+               image(img.agua, 0, 0, width, height)
+               pop()
+          }
+
+          function dibujarLoto() {
+               push()
+               applyMatrix(
+                    1 + sin(frameCount / 40) / 40, 0,
+                    0, 1 + sin(frameCount / 40) / 40,
+                    width / 2 - 130 / 2, height / 2 - 100 / 2
+               )
+               image(img.loto, 0, 0, 130, 100)
+               pop()
+          }
+     </script>
 </body>
+
 </html>
