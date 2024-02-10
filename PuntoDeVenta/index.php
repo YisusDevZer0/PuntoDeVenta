07fd7749af2d8111bedb5b5f76bc2f08e5ef2869
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-02-10 14:57:07 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -7,11 +7,11 @@
 </head>
 
 <body>
-     <midi-player src="https://bitmidi.com/uploads/112988.mid"> </midi-player>
      <script>
 
           function preload() {
                img = {
+                    suelo: loadImage("https://i.ibb.co/pJDjpBj/01.jpg"),
                     pez: loadImage("https://i.ibb.co/phvXBP8/fish-unscreen-1.gif"),
                     agua: loadImage("https://i.ibb.co/NTvPHG4/CK2ea0R.gif"),
                     loto: loadImage("https://i.ibb.co/0CrhhhV/pixlr-bg-result.png"),
@@ -24,20 +24,23 @@
           hojas = []
 
           function setup() {
-               createCanvas(800, 500)
+               createCanvas(900, 510)
                img.agua.actualFrame = 0
                img.pez.actualFrame = 0
-               for (let i = 0; i < 10; i++) {
+               for (let i = 0; i < 15; i++) {
                     peces.push(new Pez())
                }
-               let quiebres = 3
-               //lotos.push(new Loto(width / 2, height / 2))
+               let quiebres = 2
+               let l = new Loto(width / 2, height / 2)
+               l.dibujar = () => {
+               }
+               lotos.push(l)
                for (let i = 0; i < quiebres; i++) {
                     let x = 250 * cos(i * 2 * PI / quiebres) + width / 2
                     let y = 200 * sin(i * 2 * PI / quiebres) + height / 2
                     lotos.push(new Loto(x, y))
                }
-               for (let i = 0; i < 10; i++) {
+               for (let i = 0; i < 15; i++) {
                     hojas.push({
                          x: random(width),
                          y: random(height),
@@ -48,7 +51,8 @@
           }
 
           function draw() {
-               background("white")
+               image(img.suelo, 0, 0, width, height)
+               background(0, 0, 0, 50)
                dibujarPeces()
                dibujarAgua()
                dibujarHojas()
@@ -58,15 +62,36 @@
           function dibujarHojas() {
                for (const h of hojas) {
                     push()
-                    h.x += 0.2 * random(-1, 1)
-                    h.y += 0.2 * random(-1, 1)
+                    h.x += 0.3 * random(-1, 1) + random(0.5)
+                    h.y += 0.3 * random(-1, 1) + random(0.5)
                     translate(h.x, h.y)
                     rotate(h.angulo)
                     scale(h.escala)
                     scale(1 + sin(frameCount / 20) / 10)
-                    drawingContext.filter = "drop-shadow(20px 20px 20px rgba(0,0,0,0.5))"
+                    drawingContext.filter = "drop-shadow(20px 20px 20px rgba(0,0,0,1))"
                     image(img.hoja, 0, 0)
                     pop()
+               }
+
+               for (const h of hojas) {
+                    if (h.x > width + 30 || h.y > height + 30) {
+                         hojas.splice(hojas.indexOf(h), 1)
+                         if (random() < 0.5) {
+                              hojas.push({
+                                   x: -30,
+                                   y: random(height),
+                                   escala: random(0.05, 0.2),
+                                   angulo: random(2 * PI)
+                              })
+                         } else {
+                              hojas.push({
+                                   x: random(width),
+                                   y: -30,
+                                   escala: random(0.05, 0.2),
+                                   angulo: random(2 * PI)
+                              })
+                         }
+                    }
                }
           }
 
@@ -80,19 +105,24 @@
                constructor(x, y) {
                     this.x = x
                     this.y = y
+                    this.escala = 0.15
                }
 
                dibujar() {
-                    this.x += 0.3 * random(-1, 1)
-                    this.y += 0.3 * random(-1, 1)
+                    let pos = createVector(this.x, this.y)
+                         .sub(width / 2, height / 2)
+                         .rotate(0.001)
+                         .add(width / 2, height / 2)
+                         .add(0.3 * random(-1, 1), 0.3 * random(-1, 1))
+                    this.x = pos.x
+                    this.y = pos.y
                     push()
-                    applyMatrix(
-                         1 + sin(frameCount / 20) / 40, 0,
-                         0, 1 + sin(frameCount / 20) / 40,
-                         this.x - 130 / 2, this.y - 100 / 2
-                    )
-                    drawingContext.filter = "drop-shadow(20px 20px 20px rgba(0,0,0,0.5))"
-                    image(img.loto, 0, 0, 130, 100)
+                    translate(this.x, this.y)
+                    translate(-img.loto.width * this.escala / 2, -img.loto.height * this.escala / 2)
+                    scale(this.escala * (1 + sin(frameCount / 20) / 40))
+
+                    drawingContext.filter = "drop-shadow(20px 20px 20px rgba(0,0,0,1))"
+                    image(img.loto, 0, 0)
                     pop()
                }
           }
@@ -105,14 +135,14 @@
                     this.aceleracion = createVector(
                          -sin(angulo_inicio), cos(angulo_inicio)
                     ).mult((random() < 0.5 ? -1 : 1) * random(100, 400))
-                    this.escala = random(0.2, 0.5)
+                    this.escala = random(0.2, 0.6)
                     this.hue = random(0, 720)
                }
 
                dibujar() {
                     push()
                     let m = this.aceleracion.copy().mag()
-                    this.posicion.add(this.aceleracion.copy().normalize().mult(random(1, 2)))
+                    this.posicion.add(this.aceleracion.copy().normalize().mult(5 * noise(this.posicion.x, this.posicion.y, frameCount / 10)))
                     for (const l of lotos) {
                          let d = this.posicion.dist(createVector(l.x, l.y))
                          this.aceleracion.add(
@@ -121,7 +151,7 @@
                                         this.posicion.y - l.y,
                                         this.posicion.x - l.x
                                    ),
-                                   -(d/600)
+                                   -(d / 600)
                               )
                          )
                     }
@@ -129,7 +159,7 @@
                     rotate(this.aceleracion.heading() - PI / 2)
                     scale(this.escala, this.escala)
                     translate(-img.pez.width / 2, -img.pez.height)
-                    drawingContext.filter = `hue-rotate(${this.hue}deg) drop-shadow(5px 5px 10px rgba(0,0,0,0.5))`
+                    drawingContext.filter = `hue-rotate(${this.hue}deg) drop-shadow(10px 10px 10px black)`
                     img.pez.setFrame(int(img.pez.actualFrame))
                     img.pez.actualFrame += 0.2
                     img.pez.actualFrame %= img.pez.numFrames()
@@ -140,9 +170,10 @@
 
           function dibujarAgua() {
                push()
-               drawingContext.globalAlpha = 0.4
+               drawingContext.globalCompositeOperation = "soft-light"
+               //drawingContext.globalAlpha = 0.3
                img.agua.setFrame(int(img.agua.actualFrame))
-               img.agua.actualFrame += 0.5
+               img.agua.actualFrame += 0.6
                img.agua.actualFrame %= img.agua.numFrames()
                image(img.agua, 0, 0, width, height)
                pop()
