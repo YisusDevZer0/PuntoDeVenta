ee1b7579767b78e8c0ed14fabe6425e1a90a9889
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-02-11 10:35:03 -0600

    Update Sucursales.php

--- PuntoDeVenta/ControlYAdministracion/Sucursales.php
+++ PuntoDeVenta/ControlYAdministracion/Sucursales.php
@@ -7,7 +7,7 @@ include_once "Controladores/ControladorUsuario.php";
 
 <head>
     <meta charset="utf-8">
-    <title>Personal activo <?php echo $row['Licencia']?></title>
+    <title>Sucursales de <?php echo $row['Licencia']?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
