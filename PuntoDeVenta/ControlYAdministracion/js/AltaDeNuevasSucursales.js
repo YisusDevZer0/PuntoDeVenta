be5c197f63e8523e9182925d391ec35c20d54ec3
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-02-11 11:37:10 -0600

    Update AltaDeNuevasSucursales.js

--- PuntoDeVenta/ControlYAdministracion/js/AltaDeNuevasSucursales.js
+++ PuntoDeVenta/ControlYAdministracion/js/AltaDeNuevasSucursales.js
@@ -62,7 +62,7 @@ VigenciaProdT: {
   
         $.ajax({				
             type : 'POST',
-            url: "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/AltaNuevaSucursal.php",
+            url: "Controladores/AltaNuevaSucursal.php",
             data: $('#NewTypeUser').serialize(),
             cache: false,
             beforeSend: function(){	
