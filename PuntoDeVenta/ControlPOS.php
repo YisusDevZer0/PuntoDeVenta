6414321299783ef6c833ae99277c12b99f8c056b
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-02-29 19:45:36 -0600

    Actualizacion de validacion de tipos de usuarios

--- PuntoDeVenta/ControlPOS.php
+++ PuntoDeVenta/ControlPOS.php
@@ -10,3 +10,40 @@ if($_SESSION["ControlMaestro"])	//Condicion admin
 
 }
 
+if($_SESSION["Vendedor"])	//Condicion admin
+{
+	
+
+	header("location:https://doctorpez.mx/PuntoDeVenta/Ventas/");	
+
+}
+
+if($_SESSION["Administrador"])	//Condicion admin
+{
+	
+
+	header("location:https://doctorpez.mx/PuntoDeVenta/Administracion/");	
+
+}
+
+if($_SESSION["Supervision"])	//Condicion admin
+{
+	
+
+	header("location:https://doctorpez.mx/PuntoDeVenta/Supervision/");	
+
+}
+if($_SESSION["Inventarios"])	//Condicion admin
+{
+	
+
+	header("location:https://doctorpez.mx/PuntoDeVenta/Inventarios/");	
+
+}
+if($_SESSION["RecursosHumanos"])	//Condicion admin
+{
+	
+
+	header("location:https://doctorpez.mx/PuntoDeVenta/RecursosHumanos/");	
+
+}
