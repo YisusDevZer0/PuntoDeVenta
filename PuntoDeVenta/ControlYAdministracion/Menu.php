80549614aafee64c42e307e6cfd4bc6a5bf0cc62
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-02 14:07:21 -0600

    Update Menu.php

--- PuntoDeVenta/ControlYAdministracion/Menu.php
+++ PuntoDeVenta/ControlYAdministracion/Menu.php
@@ -38,7 +38,7 @@
                     </div>
                    
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-cash-register"></i></i></i>Clientes</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-users"></i>Clientes</a>
                         <div class="dropdown-menu bg-transparent border-0">
                             <a href="FondosDeCaja" class="dropdown-item"><i class="fa-solid fa-people-group"></i>Data de clientes</a>
                             <a href="CajasActivas" class="dropdown-item"><i class="fa-regular fa-credit-card"></i>Creditos </a>
