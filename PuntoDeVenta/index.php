98f5592a73b61caecfddeac12ed877e12c31c17f
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-01 19:05:49 -0600

    Update index.php

--- PuntoDeVenta/index.php
+++ PuntoDeVenta/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PUNTO DE VENTA</title>
+    <title>🔒 Inicio de sesión | PUNTO DE VENTA 🐟</title>
     <!-- Agrega los estilos de Material Design -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
