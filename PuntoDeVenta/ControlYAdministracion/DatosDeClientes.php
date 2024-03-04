1287b6c5a526213b6d76f85fb7a8f1e5163d6a80
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-03 21:46:59 -0600

    Update DatosDeClientes.php

--- PuntoDeVenta/ControlYAdministracion/DatosDeClientes.php
+++ PuntoDeVenta/ControlYAdministracion/DatosDeClientes.php
@@ -7,7 +7,7 @@ include_once "Controladores/ControladorUsuario.php";
 
 <head>
     <meta charset="utf-8">
-    <title style="">Clientes registrados para  <?php echo $row['Licencia']?></title>
+    <title style="color:#0172b6;">Clientes registrados para  <?php echo $row['Licencia']?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
    
 
