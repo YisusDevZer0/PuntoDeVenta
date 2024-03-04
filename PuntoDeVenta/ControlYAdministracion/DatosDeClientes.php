13f7715a69403f24c67eb0305b0032ada0421759
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-03 21:52:56 -0600

    Update DatosDeClientes.php

--- PuntoDeVenta/ControlYAdministracion/DatosDeClientes.php
+++ PuntoDeVenta/ControlYAdministracion/DatosDeClientes.php
@@ -7,7 +7,7 @@ include_once "Controladores/ControladorUsuario.php";
 
 <head>
     <meta charset="utf-8">
-    <title>Clientes de <?php echo $row['Licencia']?></title>
+    <title>Fondos de cajas disponibles para  <?php echo $row['Licencia']?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
    
 
