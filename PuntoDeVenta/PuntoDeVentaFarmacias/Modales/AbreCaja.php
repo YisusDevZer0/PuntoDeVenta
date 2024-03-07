eaceba6dc60db348a55625893a2d4af6f1037647
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-06 22:49:46 -0600

    Update AbreCaja.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/AbreCaja.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/AbreCaja.php
@@ -5,7 +5,10 @@ include "../Controladores/ControladorUsuario.php";
 
 $fcha = date("Y-m-d");
 $user_id=null;
-$sql1= "SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja,Fondos_Cajas.Licencia, Fondos_Cajas.Estatus, Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM Fondos_Cajas,Sucursales where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal AND Fondos_Cajas.Licencia'".$row['Licencia']."' AND Fondos_Cajas.ID_Fon_Caja = ".$_POST["id"];
+$sql1= "SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja,Fondos_Cajas.Licencia, 
+Fondos_Cajas.Estatus, Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM Fondos_Cajas,Sucursales 
+where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal AND Fondos_Cajas.Licencia'".$row['Licencia']."' 
+AND Fondos_Cajas.ID_Fon_Caja = ".$_POST["id"];
 $query = $conn->query($sql1);
 $Especialistas = null;
 if($query->num_rows>0){
