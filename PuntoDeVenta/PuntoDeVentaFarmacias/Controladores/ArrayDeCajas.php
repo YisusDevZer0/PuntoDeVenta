3bfe01ffb125d4eda1dedd06f26fe94da69e2e34
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-06 22:55:45 -0600

    Update ArrayDeCajas.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayDeCajas.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayDeCajas.php
@@ -6,28 +6,14 @@ include_once "Controladores/ControladorUsuario.php";
 
 
 
-$sql = "SELECT 
-Cajas_POS.ID_Caja,
-Cajas_POS.Cantidad_Fondo,
-Cajas_POS.Empleado,
-Cajas_POS.Sucursal,
-Cajas_POS.Estatus,
-Cajas_POS.CodigoEstatus,
-Cajas_POS.Turno,
-Cajas_POS.Asignacion,
-Cajas_POS.Fecha_Apertura,
-Cajas_POS.Valor_Total_Caja,
-Cajas_POS.ID_H_O_D,
-Sucursales.ID_SucursalC,
-Sucursales.Nombre_Sucursal 
-FROM 
-Cajas_POS, SucursalesCorre 
-WHERE 
-Cajas_POS.Sucursal = Sucursales.ID_SucursalC 
-AND Cajas_POS.Fecha_Apertura = '$fechaActual'  -- Usa la variable de fecha
-AND Cajas_POS.Sucursal='".$row['Fk_Sucursal']."'
-AND Cajas_POS.Empleado='".$row['Nombre_Apellidos']."'
-AND Cajas_POS.ID_H_O_D='".$row['Licencia']."'";
+$sql = "SELECT Cajas.ID_Caja, Cajas.Cantidad_Fondo, Cajas.Empleado, Cajas.Sucursal,
+ Cajas.Estatus, Cajas.CodigoEstatus, Cajas.Turno, Cajas.Asignacion, Cajas.Fecha_Apertura,
+  Cajas.Valor_Total_Caja, Cajas.Licencia, Sucursales.ID_Sucursal, Sucursales.Nombre_Sucursal 
+  FROM Cajas, Sucursales WHERE Cajas.Sucursal = Sucursales.ID_Sucursal
+AND Cajas.Fecha_Apertura = '$fechaActual'  -- Usa la variable de fecha
+AND Cajas.Sucursal='".$row['Fk_Sucursal']."'
+AND Cajas.Empleado='".$row['Nombre_Apellidos']."'
+AND Cajas.Licencia='".$row['Licencia']."'";
  
 $result = mysqli_query($conn, $sql);
  
