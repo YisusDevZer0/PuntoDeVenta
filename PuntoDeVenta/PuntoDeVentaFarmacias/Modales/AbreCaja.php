a1a9abb01698b85c052bdd644ee486fcde144d65
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-09 21:03:55 -0600

    Update AbreCaja.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/AbreCaja.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/AbreCaja.php
@@ -7,8 +7,7 @@ $fcha = date("Y-m-d");
 $user_id=null;
 $sql1= "SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja,Fondos_Cajas.Licencia, 
 Fondos_Cajas.Estatus, Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM Fondos_Cajas,Sucursales 
-where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal AND 
-AND Fondos_Cajas.ID_Fon_Caja = ".$_POST["id"];
+where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal AND  Fondos_Cajas.ID_Fon_Caja = ".$_POST["id"];
 $query = $conn->query($sql1);
 $Especialistas = null;
 if($query->num_rows>0){
