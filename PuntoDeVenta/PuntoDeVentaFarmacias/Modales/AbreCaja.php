10be8d33cef957390b7e1ecb98587a87da7f3399
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-09 21:07:33 -0600

    Update AbreCaja.php

---
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/AbreCaja.php
@@ -0,0 +1,97 @@
+<?php
+date_default_timezone_set("America/Monterrey");
+include "../Controladores/db_connect.php";
+include "../Controladores/ControladorUsuario.php";
+
+$fcha = date("Y-m-d");
+$user_id=null;
+$sql1= "SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja,Fondos_Cajas.Licencia, 
+Fondos_Cajas.Estatus, Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM Fondos_Cajas,Sucursales 
+where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal AND  Fondos_Cajas.ID_Fon_Caja = ".$_POST["id"];
+$query = $conn->query($sql1);
+$Especialistas = null;
+if($query->num_rows>0){
+while ($r=$query->fetch_object()){
+  $Especialistas=$r;
+  break;
+}
+
+  }
+  $hora = date('G');
+?>
+
+<?php if($Especialistas!=null):?>
+
+<form action="javascript:void(0)" method="post" id="OpenCaja" >
+<div class="row">
+    <div class="col">
+    <label for="exampleFormControlInput1">Cantidad asignada en fondo de caja </label>
+    <div class="input-group mb-3">
+  <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
+  </div>
+  <input type="text" class="form-control " hidden name="FkFondo" id="fkfondo" readonly value="<?php echo $Especialistas->ID_Fon_Caja; ?>">
+  <input type="number" class="form-control "  id="cantidad" name="Cantidad" step="any" readonly value="<?php echo $Especialistas->Fondo_Caja; ?>" aria-describedby="basic-addon1" >  
+    </div>
+    </div>
+    
+   
+    <div class="col">
+    <label for="exampleFormControlInput1">Empleado<span class="text-danger">*</span></label>
+     <div class="input-group mb-3">
+  <div class="input-group-prepend">
+  
+    <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
+  </div>
+  <input type="text" class="form-control " readonly  name="Empleado" id="empleado" value="<?php echo $row['Nombre_Apellidos']?>" aria-describedby="basic-addon1" >            
+</div></div></div>
+
+<div class="row">
+    <div class="col">
+    <label for="exampleFormControlInput1">Sucursal </label>
+    <div class="input-group mb-3">
+  <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
+  </div>
+  <input type="text" class="form-control " readonly  value="<?php echo $Especialistas->Nombre_Sucursal; ?>" aria-describedby="basic-addon1" >       
+  <input type="text" class="form-control " readonly name="Sucursal" id="sucursal" hidden value="<?php echo $Especialistas->Fk_Sucursal; ?>" aria-describedby="basic-addon1" >       
+    </div>
+    </div>
+    
+   
+    <div class="col">
+    <label for="exampleFormControlInput1">Fecha<span class="text-danger">*</span></label>
+     <div class="input-group mb-3">
+  <div class="input-group-prepend">
+  
+    <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
+  </div>
+  <input type="text" class="form-control " readonly name="Fecha" id="fecha" value="<?php echo $fcha; ?>" aria-describedby="basic-addon1" >   
+  <input type="text" class="form-control " hidden readonly name="Asignacion" id="asignacion" value="1" aria-describedby="basic-addon1" >            
+</div></div></div>
+<div class="row">
+    <div class="col">
+    <label for="exampleFormControlInput1">Turno </label>
+    <div class="input-group mb-3">
+  <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
+  </div>
+  <select name="Turno" id="turno"  onchange="TurnoElegido();"class="form-control">
+  <option value="">Escoge un turno</option>
+ 
+  <option value="Matutino">Matutino</option>
+  <option value="Vespertino">Vespertino</option>
+  <option value="Nocturno">Nocturno</option>
+  </select>      
+    </div>
+    </div>
+    <div class="col">
+    <label for="exampleFormControlInput1">Cantidad total en caja<span class="text-danger">*</span></label>
+     <div class="input-group mb-3">
+  <div class="input-group-prepend">
+  
+    <span class="input-group-text" id="Tarjeta"><i class="fas fa-coins"></i></span>
+  </div>
+  <input type="number" class="form-control " onfocus="habilitar();" step="any" name="TotalCaja" id="resultado" readonly   aria-describedby="basic-addon1" >     
+  </div>
+</div><label for="resultado" class="error"></div>
+<div class="" id="Ok" role="alert">
+  
+</div>
