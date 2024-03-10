126a09fab59bdc25aa838c1b3e40179b4a0aefd4
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-09 19:37:17 -0600

    Caja

---
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ConsultaFondoDeCaja.php
@@ -0,0 +1,11 @@
+<?php
+include_once("db_connection.php");
+$sql ="SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja, 
+Fondos_Cajas.Estatus, 
+Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM 
+Fondos_Cajas,Sucursales where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal
+ AND Fondos_Cajas.Fk_Sucursal='".$row['Fk_Sucursal']."' AND Fondos_Cajas.Estatus ='Activo' ";
+$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+$ValorFondoCaja = mysqli_fetch_assoc($resultset);
+
+?>
