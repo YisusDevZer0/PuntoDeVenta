a7c7e22557d39f51f9bab3da4868f58fac17fcf3
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-09 19:56:40 -0600

    Update AperturarCaja.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/AperturarCaja.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/AperturarCaja.php
@@ -1,6 +1,12 @@
 <?php
 include_once "Controladores/ControladorUsuario.php";
-include_once "Controladores/ConsultaFondoCaja.php";
+$sql ="SELECT Fondos_Cajas.ID_Fon_Caja,Fondos_Cajas.Fk_Sucursal,Fondos_Cajas.Fondo_Caja, 
+Fondos_Cajas.Estatus, 
+Sucursales.ID_Sucursal,Sucursales.Nombre_Sucursal FROM 
+Fondos_Cajas,Sucursales where Fondos_Cajas.Fk_Sucursal = Sucursales.ID_Sucursal
+ AND Fondos_Cajas.Fk_Sucursal='".$row['Fk_Sucursal']."' AND Fondos_Cajas.Estatus ='Activo' ";
+$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+$ValorFondoCaja = mysqli_fetch_assoc($resultset);
 ?>
 <!DOCTYPE html>
 <html lang="es">
