6346be77cec9d4949a98d1309b8880c3bffa96b7
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-10 19:12:28 -0600

    Update AperturaCaja.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/AperturaCaja.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/AperturaCaja.php
@@ -18,12 +18,8 @@ $Hora_real_apertura = $conn->real_escape_string(htmlentities(strip_tags(trim($_P
 $Sistema = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['Sistema']))));
 $Licencia = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['Licencia']))));
 
-// Verificar si ya existe un registro con los mismos valores
-$sql = "SELECT Empleado,Sucursal,Estatus,Fecha_Apertura,Licencia,Cantidad_Fondo,Turno,Asignacion FROM Cajas_POS
-        WHERE Empleado='$Empleado' AND Sucursal='$Sucursal' AND Estatus='$Estatus' AND Fecha_Apertura='$Fecha_Apertura' AND Cantidad_Fondo='$Cantidad_Fondo'AND Turno='$Turno' 
-        AND Asignacion='$Asignacion' AND Licencia='$Licencia'";
-$resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
-$row = mysqli_fetch_assoc($resultset);
+
+
 
 // Verificar si ya existe un registro con los mismos valores
 if ($row && $row['Empleado'] == $Empleado && $row['Sucursal'] == $Sucursal && $row['Estatus'] == $Estatus && $row['Fecha_Apertura'] == $Fecha_Apertura
