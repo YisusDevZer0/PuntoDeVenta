b6892dc149e5b9f4e9d54d26a5c14019eec9489b
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-13 19:35:38 -0600

    Update Servicios.php

--- PuntoDeVenta/ControlYAdministracion/Servicios.php
+++ PuntoDeVenta/ControlYAdministracion/Servicios.php
@@ -33,9 +33,9 @@ include_once "Controladores/ControladorUsuario.php";
 
             <!-- Table Start -->
           
-           
+            <div class="container-fluid pt-4 px-4">
     <div class="col-12">
-        
+        <div class="bg-light rounded h-100 p-4">
             <h6 class="mb-4" style="color:#0172b6;">Servicios <?php echo $row['Licencia']?></h6>
             
             <div id="DataDeServicios"></div>
