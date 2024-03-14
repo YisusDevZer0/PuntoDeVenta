5f4f0d3aa511271fbab980c7fe87529b01df87fe
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-13 21:19:31 -0600

    Update ArrayServicios.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/ArrayServicios.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/ArrayServicios.php
@@ -38,7 +38,7 @@ while ($fila = $result->fetch_assoc()) {
         "Sistema" => $fila["Sistema"],
         "Licencia" => $fila["Licencia"],
         // Agregar el botón Desglosar ticket
-        "Editar" => '<td><a data-id="' . $fila["Servicio_ID"] . '" class="btn btn-success btn-sm btn-desglose dropdown-item" style="background-color: #0172b6 !important;" ><i class="fa-solid fa-pen-to-square"></i>Desglosar ticket</a></td>'
+        "Editar" => '<td><a data-id="' . $fila["Servicio_ID"] . '" class="btn btn-success btn-sm btn-desglose dropdown-item" style="background-color: #0172b6 !important;" ><i class="fa-solid fa-pen-to-square"></i></a></td>'
 
     ];
 }
