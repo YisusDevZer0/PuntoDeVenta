844276c679411edf7974bcd72a172b9fcce01f56
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-13 22:16:29 -0600

    Update NuevoServicio.php

--- PuntoDeVenta/ControlYAdministracion/Modales/NuevoServicio.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/NuevoServicio.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Agregar Nueva sucursal</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Agregar Nuevo servicio</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -16,16 +16,10 @@
                   <!-- Agrega un campo oculto para el token CSRF -->
     
                 <div class="mb-3">
-                  <label for="tipouser" class="form-label">Cantidad del fondo de caja</label>
+                  <label for="tipouser" class="form-label">Nombre del servicio</label>
                   <input type="text" class="form-control" name="NombreSucursal" id="tipoUsuario" placeholder="Ingrese el tipo de usuario">
                 </div>
-                <div class="mb-3">
-                  <label for="tipouser" class="form-label">Sucursal</label>
-                  <select id="opciones" class="form-select form-select-sm mb-3" aria-label=".form-select-sm example">
-    <option selected>Selecciona una opción</option>
-</select>
-
-                </div>
+                
                
                 
                 <input type="text" hidden class="form-control" name="licencia" id ="licencia" value="<?php echo $row['Licencia']?>">
