e8202c070908bc683ac010bea69c9fc38dd5afc5
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-23 09:10:25 -0600

    Update EliminarServicio.php

--- PuntoDeVenta/ControlYAdministracion/Modales/EliminarServicio.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/EliminarServicio.php
@@ -21,7 +21,7 @@ while ($r=$query->fetch_object()){
 <i class="fas fa-question-circle fa-5x text-warning"></i>
         <p>¿Está seguro de que desea eliminar el servicio <?php echo $Especialistas->Nom_Serv; ?> ?</p>
 <input type="hidden" name="Id_Serv" id="id" value="<?php echo $Especialistas->Servicio_ID; ?>">
-<button type="submit"  id="submit"  class="btn btn-info">Aplicar cambios <i class="fas fa-check"></i></button>
+<button type="submit"  id="submit"  class="btn btn-danger">Confirmar<i class="fas fa-check"></i></button>
                           
 </form>
 <script src="js/ActualizacionDeServicios.js"></script>
