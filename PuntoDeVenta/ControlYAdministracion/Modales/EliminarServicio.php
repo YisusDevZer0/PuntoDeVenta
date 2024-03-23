ee1572775cbfeab420ad7f9b8d181857a2050b4b
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-23 09:27:22 -0600

    Update EliminarServicio.php

--- PuntoDeVenta/ControlYAdministracion/Modales/EliminarServicio.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/EliminarServicio.php
@@ -17,7 +17,7 @@ while ($r=$query->fetch_object()){
 
 <?php if($Especialistas!=null):?>
 
-<form action="javascript:void(0)" method="post" id="EliminaServicios" >
+<form action="javascript:void(0)" method="post" id="NewTypeUser" >
 <i class="fas fa-question-circle fa-5x text-warning"></i>
         <p>¿Está seguro de que desea eliminar el servicio <?php echo $Especialistas->Nom_Serv; ?> ?</p>
 <input type="hidden" name="Id_Serv" id="id" value="<?php echo $Especialistas->Servicio_ID; ?>">
