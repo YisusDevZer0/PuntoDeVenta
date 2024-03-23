9cab8f44a8922d8c50104f9a6f3c15952debf330
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-23 09:06:38 -0600

    Create EliminarServicio.php

---
+++ PuntoDeVenta/ControlYAdministracion/Modales/EliminarServicio.php
@@ -0,0 +1,31 @@
+<?php
+include "../Controladores/db_connect.php.php";
+include "../Controladores/ControladorUsuario.php";
+
+$user_id=null;
+$sql1= "SELECT * FROM Servicios_POS WHERE Licencia='".$row['Licencia']."' AND Servicio_ID = ".$_POST["id"];
+$query = $conn->query($sql1);
+$Especialistas = null;
+if($query->num_rows>0){
+while ($r=$query->fetch_object()){
+  $Especialistas=$r;
+  break;
+}
+
+  }
+?>
+
+<?php if($Especialistas!=null):?>
+
+<form action="javascript:void(0)" method="post" id="ActualizaServicios" >
+<i class="fas fa-question-circle fa-5x text-warning"></i>
+        <p>¿Está seguro de que desea eliminar este contenido?</p>
+<input type="hidden" name="Id_Serv" id="id" value="<?php echo $Especialistas->Servicio_ID; ?>">
+<button type="submit"  id="submit"  class="btn btn-info">Aplicar cambios <i class="fas fa-check"></i></button>
+                          
+</form>
+<script src="js/ActualizacionDeServicios.js"></script>
+
+<?php else:?>
+  <p class="alert alert-danger">404 No se encuentra</p>
+<?php endif;?>
