4cb78e4850f1e58c26b9f59ceb71b07662281660
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-23 12:18:37 -0600

    Presentaciones

--- PuntoDeVenta/ControlYAdministracion/Modales/EliminarPresentacion.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/EliminarPresentacion.php
@@ -3,7 +3,7 @@ include "../Controladores/db_connect.php.php";
 include "../Controladores/ControladorUsuario.php";
 
 $user_id=null;
-$sql1= "SELECT * FROM Marcas_POS WHERE Licencia='".$row['Licencia']."' AND Marca_ID = ".$_POST["id"];
+$sql1= "SELECT * FROM Presentaciones WHERE Licencia='".$row['Licencia']."' AND Presentacion_ID = ".$_POST["id"];
 $query = $conn->query($sql1);
 $Especialistas = null;
 if($query->num_rows>0){
@@ -19,12 +19,12 @@ while ($r=$query->fetch_object()){
 
 <form action="javascript:void(0)" method="post" id="EliminaServiciosForm" >
 <i class="fas fa-question-circle fa-5x text-warning"></i>
-        <p>¿Está seguro de que desea eliminar el servicio <?php echo $Especialistas->Nom_Marca; ?> ?</p>
-<input type="hidden" name="Id_Serv" id="id" value="<?php echo $Especialistas->Marca_ID; ?>">
+        <p>¿Está seguro de que desea eliminar el servicio <?php echo $Especialistas->Nom_Presentacion; ?> ?</p>
+<input type="hidden" name="Id_Serv" id="id" value="<?php echo $Especialistas->Presentacion_ID; ?>">
 <button type="submit"  id="submit"  class="btn btn-danger">Confirmar<i class="fas fa-check"></i></button>
                           
 </form>
-<script src="js/EliminarMarcass.js"></script>
+<script src="js/EliminarPresentacion.js"></script>
 
 <?php else:?>
   <p class="alert alert-danger">404 No se encuentra</p>
