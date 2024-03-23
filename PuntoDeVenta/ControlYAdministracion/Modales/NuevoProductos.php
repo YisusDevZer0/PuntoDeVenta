5dd53dd9dca82e6fdd1b3ddb76d876d555fb63cc
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-22 18:30:03 -0600

    Update NuevoProductos.php

--- PuntoDeVenta/ControlYAdministracion/Modales/NuevoProductos.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/NuevoProductos.php
@@ -3,8 +3,8 @@
 <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl modal-notify modal-success">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Agregar Nuevo Producto</h5>
+      <div class="modal-header" style=" background-color: #C80096 !important;">
+        <h5 class="modal-title" style="color:white;" id="exampleModalLabel">Agregar Nuevo Producto</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="text-center">
