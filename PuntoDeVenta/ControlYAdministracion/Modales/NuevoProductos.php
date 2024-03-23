b2da0a78b5d0b10b25465e441735f07fa63b773c
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-22 18:13:40 -0600

    Update NuevoProductos.php

--- PuntoDeVenta/ControlYAdministracion/Modales/NuevoProductos.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/NuevoProductos.php
@@ -18,8 +18,6 @@
     <div class="col">
     <label for="exampleFormControlInput1">Codigo de barras<span class="text-danger">*</span> </label>
     <div class="input-group mb-3">
-  <div class="input-group-prepend">  <span class="input-group-text" ><i class="fas fa-barcode"></i></span>
-  </div>
   <input type="text" class="form-control " id="codbarrap" name="CodBarraP" placeholder="Escanee o ingrese código" >
     </div>
     </div>
