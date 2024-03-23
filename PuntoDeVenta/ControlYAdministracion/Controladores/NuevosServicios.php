e01e637e68f6a709708d8a03e5bdcb78e5e3eadc
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-23 07:23:28 -0600

    Update NuevosServicios.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/NuevosServicios.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/NuevosServicios.php
@@ -1,34 +1,29 @@
 <?php
     include_once 'db_connect.php';
 
-    $Servicio = $_POST['NombreServicio'];
-    $Estado = $_POST['estado'];
-    $Creado = $_POST['agrego'];
-    $Sistema = $_POST['Sistema'];
-    $Licencia = $_POST['licencia'];
-    
+    $NomServ = mysqli_real_escape_string($conn, $_POST['NomServ']);
+    $Estado = mysqli_real_escape_string($conn, $_POST['estado']);
+    $AgregadoPor = mysqli_real_escape_string($conn, $_POST['agregoPor']);
+    $Sistema = mysqli_real_escape_string($conn, $_POST['sistema']);
+    $Licencia = mysqli_real_escape_string($conn, $_POST['licencia']);
   
-//include database configuration file
-    
-    $sql = "SELECT Nom_Serv,Licencia,Creado FROM Servicios_POS WHERE Nom_Serv='$Servicio' AND Licencia='$Licencia'";
+    // Consulta para verificar si ya existe un registro con los mismos valores
+    $sql = "SELECT Nom_Serv, Licencia, Agregadoel FROM Servicios_POS WHERE Nom_Serv='$NomServ' AND Licencia='$Licencia'";
     $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-    $row = mysqli_fetch_assoc($resultset);	
-        //include database configuration file
-        if($row['Nom_Serv']==$Servicio  AND $row['Licencia']==$Licencia){				
-            echo json_encode(array("statusCode"=>250));
-          
-        } 
-        else{
-            $sql = "INSERT INTO `Servicios_POS`( `Nom_Serv`, `Estado`, `Agregado_Por`,  `Sistema`, `Licencia`) 
-            VALUES ('$Servicio','$Estado','$Creado','$Sistema','$Licencia')";
+    $row = mysqli_fetch_assoc($resultset);
+    
+    if(mysqli_num_rows($resultset) > 0) {
+        echo json_encode(array("statusCode"=>250)); // El registro ya existe
+    } else {
+        // Consulta de inserción para agregar un nuevo registro
+        $sql = "INSERT INTO `Servicios_POS`(`Nom_Serv`, `Estado`, `Agregado_Por`, `Sistema`, `Licencia`) 
+                VALUES ('$NomServ', '$Estado', '$AgregadoPor', '$Sistema', '$Licencia')";
         
-            if (mysqli_query($conn, $sql)) {
-                echo json_encode(array("statusCode"=>200));
-            } 
-            else {
-                echo json_encode(array("statusCode"=>201));
-            }
-            mysqli_close($conn);
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("statusCode"=>200)); // Inserción exitosa
+        } else {
+            echo json_encode(array("statusCode"=>201)); // Error en la inserción
         }
-
+        mysqli_close($conn);
+    }
 ?>
