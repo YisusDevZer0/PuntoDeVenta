eaa547535aaef60b85935d854e24bcb874b56bc0
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-24 09:11:35 -0600

    Update AltaProductos.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/AltaProductos.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/AltaProductos.php
@@ -1,41 +1,29 @@
 <?php
-include_once 'db_connect.php';
+    include_once 'db_connect.php';
 
-$Cod_Barra = mysqli_real_escape_string($conn, $_POST['CodBarraP']);
-$Clave_adicional = mysqli_real_escape_string($conn, $_POST['Clav']);
-$Clave_Levic = ''; // Este valor no está presente en el formulario, puedes asignarle un valor si lo deseas
-$Nombre_Prod = mysqli_real_escape_string($conn, $_POST['NombreProd']);
-$Precio_Venta = mysqli_real_escape_string($conn, $_POST['PV']);
-$Precio_C = mysqli_real_escape_string($conn, $_POST['PC']);
-$Tipo_Servicio = mysqli_real_escape_string($conn, $_POST['TipoServicio']);
-$Componente_Activo = mysqli_real_escape_string($conn, $_POST['ComponenteActivo']);
-$Tipo = mysqli_real_escape_string($conn, $_POST['Tip']);
-$FkCategoria = mysqli_real_escape_string($conn, $_POST['Categoria']);
-$FkMarca = mysqli_real_escape_string($conn, $_POST['Marca']);
-$FkPresentacion = mysqli_real_escape_string($conn, $_POST['Presentacion']);
-$Proveedor1 = mysqli_real_escape_string($conn, $_POST['Proveedor']);
-$RecetaMedica = mysqli_real_escape_string($conn, $_POST['Receta']);
-$AgregadoPor = mysqli_real_escape_string($conn, $_POST['AgregadoPor']);
-$AgregadoEl = ''; // Este valor no está presente en el formulario, puedes asignarle un valor si lo deseas
-$Licencia = mysqli_real_escape_string($conn, $_POST['Licencia']);
-
-// Consulta para verificar si ya existe un registro con los mismos valores
-$sql = "SELECT Cod_Barra, Clave_adicional, Clave_Levic, Nombre_Prod, Precio_Venta, Precio_C, Tipo_Servicio, Componente_Activo, Tipo, FkCategoria, FkMarca, FkPresentacion, Proveedor1, RecetaMedica, AgregadoPor, AgregadoEl, Licencia FROM Productos_POS WHERE Cod_Barra='$Cod_Barra' AND Licencia='$Licencia'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$row = mysqli_fetch_assoc($resultset);
-
-if(mysqli_num_rows($resultset) > 0) {
-    echo json_encode(array("statusCode"=>250)); // El registro ya existe
-} else {
-    // Consulta de inserción para agregar un nuevo registro
-    $sql = "INSERT INTO `Productos_POS`(`Cod_Barra`, `Clave_adicional`, `Clave_Levic`, `Nombre_Prod`, `Precio_Venta`, `Precio_C`, `Tipo_Servicio`, `Componente_Activo`, `Tipo`, `FkCategoria`, `FkMarca`, `FkPresentacion`, `Proveedor1`, `RecetaMedica`, `Estatus`, `AgregadoPor`, `AgregadoEl`, `Licencia`) 
-            VALUES ('$Cod_Barra', '$Clave_adicional', '$Clave_Levic', '$Nombre_Prod', '$Precio_Venta', '$Precio_C', '$Tipo_Servicio', '$Componente_Activo', '$Tipo', '$FkCategoria', '$FkMarca', '$FkPresentacion', '$Proveedor1', '$RecetaMedica', '$Estatus', '$AgregadoPor', '$AgregadoEl', '$Licencia')";
+    $NomMarca = mysqli_real_escape_string($conn, $_POST['NomMarca']);
+    $AgregadoPor = mysqli_real_escape_string($conn, $_POST['agregoPor']);
+    $AgregadoEl = mysqli_real_escape_string($conn, $_POST['agregoEl']);
+    $Sistema = mysqli_real_escape_string($conn, $_POST['sistema']);
+    $Licencia = mysqli_real_escape_string($conn, $_POST['licencia']);
+  
+    // Consulta para verificar si ya existe un registro con los mismos valores
+    $sql = "SELECT Nom_Marca, Licencia, Agregadoel FROM Marcas_POS WHERE Nom_Marca='$NomMarca' AND Licencia='$Licencia'";
+    $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+    $row = mysqli_fetch_assoc($resultset);
     
-    if (mysqli_query($conn, $sql)) {
-        echo json_encode(array("statusCode"=>200)); // Inserción exitosa
+    if(mysqli_num_rows($resultset) > 0) {
+        echo json_encode(array("statusCode"=>250)); // El registro ya existe
     } else {
-        echo json_encode(array("statusCode"=>201)); // Error en la inserción
+        // Consulta de inserción para agregar un nuevo registro
+        $sql = "INSERT INTO `Marcas_POS`(`Nom_Marca`, `Agregado_Por`, `Agregadoel`, `Sistema`, `Licencia`) 
+                VALUES ('$NomMarca', '$AgregadoPor', '$AgregadoEl', '$Sistema', '$Licencia')";
+        
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("statusCode"=>200)); // Inserción exitosa
+        } else {
+            echo json_encode(array("statusCode"=>201)); // Error en la inserción
+        }
+        mysqli_close($conn);
     }
-    mysqli_close($conn);
-}
 ?>
