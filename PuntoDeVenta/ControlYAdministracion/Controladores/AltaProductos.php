8c0a22980e2357c0a073a101c06d2f3bc0af2c09
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-24 09:06:55 -0600

    Update AltaProductos.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/AltaProductos.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/AltaProductos.php
@@ -3,7 +3,7 @@ include_once 'db_connect.php';
 
 $Cod_Barra = mysqli_real_escape_string($conn, $_POST['CodBarraP']);
 $Clave_adicional = mysqli_real_escape_string($conn, $_POST['Clav']);
-$Clave_Levic = ''; // Puede asignarse un valor si es necesario
+$Clave_Levic = ''; // Este valor no está presente en el formulario, puedes asignarle un valor si lo deseas
 $Nombre_Prod = mysqli_real_escape_string($conn, $_POST['NombreProd']);
 $Precio_Venta = mysqli_real_escape_string($conn, $_POST['PV']);
 $Precio_C = mysqli_real_escape_string($conn, $_POST['PC']);
@@ -14,11 +14,11 @@ $FkCategoria = mysqli_real_escape_string($conn, $_POST['Categoria']);
 $FkMarca = mysqli_real_escape_string($conn, $_POST['Marca']);
 $FkPresentacion = mysqli_real_escape_string($conn, $_POST['Presentacion']);
 $Proveedor1 = mysqli_real_escape_string($conn, $_POST['Proveedor']);
-$Proveedor2 = mysqli_real_escape_string($conn, $_POST['Prov2']);
 $RecetaMedica = mysqli_real_escape_string($conn, $_POST['Receta']);
-$Estatus = ''; // Puede asignarse un valor si es necesario
-$AgregadoPor = mysqli_real_escape_string($conn, $_POST['AgregaProductosBy']);
-$Licencia = mysqli_real_escape_string($conn, $_POST['EmpresaProductos']);
+$Estatus = ''; // Este valor no está presente en el formulario, puedes asignarle un valor si lo deseas
+$AgregadoPor = mysqli_real_escape_string($conn, $_POST['AgregadoPor']);
+$AgregadoEl = ''; // Este valor no está presente en el formulario, puedes asignarle un valor si lo deseas
+$Licencia = mysqli_real_escape_string($conn, $_POST['Licencia']);
 
 // Consulta para verificar si ya existe un registro con los mismos valores
 $sql = "SELECT Cod_Barra, Licencia FROM Productos_POS WHERE Cod_Barra='$Cod_Barra' AND Licencia='$Licencia'";
@@ -29,8 +29,8 @@ if(mysqli_num_rows($resultset) > 0) {
     echo json_encode(array("statusCode"=>250)); // El registro ya existe
 } else {
     // Consulta de inserción para agregar un nuevo registro
-    $sql = "INSERT INTO `Productos_POS`(`Cod_Barra`, `Clave_adicional`, `Clave_Levic`, `Nombre_Prod`, `Precio_Venta`, `Precio_C`, `Tipo_Servicio`, `Componente_Activo`, `Tipo`, `FkCategoria`, `FkMarca`, `FkPresentacion`, `Proveedor1`, `Proveedor2`, `RecetaMedica`, `Estatus`, `AgregadoPor`, `Licencia`) 
-            VALUES ('$Cod_Barra', '$Clave_adicional', '$Clave_Levic', '$Nombre_Prod', '$Precio_Venta', '$Precio_C', '$Tipo_Servicio', '$Componente_Activo', '$Tipo', '$FkCategoria', '$FkMarca', '$FkPresentacion', '$Proveedor1', '$Proveedor2', '$RecetaMedica', '$Estatus', '$AgregadoPor', '$Licencia')";
+    $sql = "INSERT INTO `Productos_POS`(`Cod_Barra`, `Clave_adicional`, `Clave_Levic`, `Nombre_Prod`, `Precio_Venta`, `Precio_C`, `Tipo_Servicio`, `Componente_Activo`, `Tipo`, `FkCategoria`, `FkMarca`, `FkPresentacion`, `Proveedor1`, `RecetaMedica`, `Estatus`, `AgregadoPor`, `AgregadoEl`, `Licencia`) 
+            VALUES ('$Cod_Barra', '$Clave_adicional', '$Clave_Levic', '$Nombre_Prod', '$Precio_Venta', '$Precio_C', '$Tipo_Servicio', '$Componente_Activo', '$Tipo', '$FkCategoria', '$FkMarca', '$FkPresentacion', '$Proveedor1', '$RecetaMedica', '$Estatus', '$AgregadoPor', '$AgregadoEl', '$Licencia')";
     
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array("statusCode"=>200)); // Inserción exitosa
