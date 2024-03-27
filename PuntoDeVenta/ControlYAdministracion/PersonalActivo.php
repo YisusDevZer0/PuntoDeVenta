e3d605b272789bca036a94f0a69e6c2406e14555
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-03-26 19:49:49 -0600

    Agregar

--- PuntoDeVenta/ControlYAdministracion/PersonalActivo.php
+++ PuntoDeVenta/ControlYAdministracion/PersonalActivo.php
@@ -46,7 +46,7 @@ include_once "Controladores/ControladorUsuario.php";
             </div>
           
 <script src="js/ControlDeUsuarios.js"></script>
-<script src="js/AgregarUsuarios.js"></script>
+<script src="js/AgregarUsuarioss.js"></script>
 
             <!-- Footer Start -->
             <?php 
