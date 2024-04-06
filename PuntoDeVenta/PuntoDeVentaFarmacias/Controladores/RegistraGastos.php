844b0919464939aec418c2984c3f6fc086e6ffaf
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-04-05 22:24:45 -0600

    Update RegistraGastos.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/RegistraGastos.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/RegistraGastos.php
@@ -11,23 +11,4 @@ $Sistema = mysqli_real_escape_string($conn, $_POST['Sistema']);
 $AgregadoPor = mysqli_real_escape_string($conn, $_POST['AgregadoPor']); // Corregido aquí
 $Licencia = mysqli_real_escape_string($conn, $_POST['Licencia']);
 
-// Consulta para verificar si ya existe un registro con los mismos valores
-$sql = "SELECT Concepto_Categoria, Licencia FROM GastosPOS WHERE Concepto_Categoria='$Concepto_Categoria' AND Licencia='$Licencia'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$row = mysqli_fetch_assoc($resultset);
 
-if(mysqli_num_rows($resultset) > 0) {
-    echo json_encode(array("statusCode"=>250)); // El registro ya existe
-} else {
-    // Consulta de inserción para agregar un nuevo registro
-    $sql = "INSERT INTO `GastosPOS`(`Concepto_Categoria`, `Importe_Total`, `Empleado`, `Fk_sucursal`, `Fk_Caja`, `Recibe`, `Sistema`, `Agregado_Por`, `Licencia`) 
-            VALUES ('$Concepto_Categoria', '$Importe_Total', '$Empleado', '$FkSucursal', '$FkCaja', '$Recibe', '$Sistema', '$AgregadoPor', '$Licencia')";
-
-    if (mysqli_query($conn, $sql)) {
-        echo json_encode(array("statusCode"=>200)); // Inserción exitosa
-    } else {
-        echo json_encode(array("statusCode"=>201)); // Error en la inserción
-    }
-    mysqli_close($conn);
-}
-?>
