d99309f8b8c16a30d781303bc7d889e014bb3335
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-04-05 22:24:25 -0600

    Update RegistraGastos.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/RegistraGastos.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/RegistraGastos.php
@@ -1,8 +1,8 @@
 <?php
 include_once 'db_connect.php';
 
-$ConceptoCategoria = mysqli_real_escape_string($conn, $_POST['Concepto_Categoria']);
-$ImporteTotal = mysqli_real_escape_string($conn, $_POST['Importe_Total']);
+$Concepto_Categoria = mysqli_real_escape_string($conn, $_POST['Concepto_Categoria']);
+$Importe_Total = mysqli_real_escape_string($conn, $_POST['Importe_Total']);
 $Empleado = mysqli_real_escape_string($conn, $_POST['Empleado']);
 $FkSucursal = mysqli_real_escape_string($conn, $_POST['Fk_sucursal']);
 $FkCaja = mysqli_real_escape_string($conn, $_POST['Fk_Caja']);
@@ -12,7 +12,7 @@ $AgregadoPor = mysqli_real_escape_string($conn, $_POST['AgregadoPor']); // Corre
 $Licencia = mysqli_real_escape_string($conn, $_POST['Licencia']);
 
 // Consulta para verificar si ya existe un registro con los mismos valores
-$sql = "SELECT Concepto_Categoria, Licencia FROM GastosPOS WHERE Concepto_Categoria='$ConceptoCategoria' AND Licencia='$Licencia'";
+$sql = "SELECT Concepto_Categoria, Licencia FROM GastosPOS WHERE Concepto_Categoria='$Concepto_Categoria' AND Licencia='$Licencia'";
 $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
 $row = mysqli_fetch_assoc($resultset);
 
@@ -21,7 +21,7 @@ if(mysqli_num_rows($resultset) > 0) {
 } else {
     // Consulta de inserción para agregar un nuevo registro
     $sql = "INSERT INTO `GastosPOS`(`Concepto_Categoria`, `Importe_Total`, `Empleado`, `Fk_sucursal`, `Fk_Caja`, `Recibe`, `Sistema`, `Agregado_Por`, `Licencia`) 
-            VALUES ('$ConceptoCategoria', '$ImporteTotal', '$Empleado', '$FkSucursal', '$FkCaja', '$Recibe', '$Sistema', '$AgregadoPor', '$Licencia')";
+            VALUES ('$Concepto_Categoria', '$Importe_Total', '$Empleado', '$FkSucursal', '$FkCaja', '$Recibe', '$Sistema', '$AgregadoPor', '$Licencia')";
 
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array("statusCode"=>200)); // Inserción exitosa
