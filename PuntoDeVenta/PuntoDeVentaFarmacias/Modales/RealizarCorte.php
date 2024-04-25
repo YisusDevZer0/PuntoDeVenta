a0f09d6fd422ffb8ce0cadd2893450a13dd263aa
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-04-24 23:10:38 -0600

    Update RealizarCorte.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
@@ -2,7 +2,7 @@
 
   include "../Controladores/db_connect.php.php";
   include "../Controladores/ControladorUsuario.php";
-  include "../Controladores/ConsultaCaja.php";
+ 
   $user_id = null;
   $sql1 = "SELECT Venta_POS_ID,Folio_Ticket,Fk_Caja,Fk_sucursal FROM Ventas_POS WHERE Fk_Caja" . $_POST["id"];
   $query = $conn->query($sql1);
