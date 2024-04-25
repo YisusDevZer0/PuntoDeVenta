239240436b6ab21cf7ec610961372a3e64e4293e
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-04-24 23:09:56 -0600

    Update RealizarCorte.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
@@ -1,19 +1,15 @@
 <?php
-include "../Controladores/db_connect.php.php";
-include "../Controladores/ConsultaCaja.php";
-$fcha = date("Y-m-d");
-$user_id=null;
-$sql1= "SELECT Venta_POS_ID,Folio_Ticket,Fk_Caja,Fk_sucursal FROM Ventas_POS WHERE Fk_Caja = '".$_POST['id']."'   order by  Venta_POS_ID ASC limit 1";
-$query = $conn->query($sql1);
-$Especialistas = null;
-if($query->num_rows>0){
-while ($r=$query->fetch_object()){
-  $Especialistas=$r;
-  break;
-}
 
-  }
- 
-
+  include "../Controladores/db_connect.php.php";
+  include "../Controladores/ControladorUsuario.php";
+  include "../Controladores/ConsultaCaja.php";
+  $user_id = null;
+  $sql1 = "SELECT Venta_POS_ID,Folio_Ticket,Fk_Caja,Fk_sucursal FROM Ventas_POS WHERE Fk_Caja" . $_POST["id"];
+  $query = $conn->query($sql1);
+  $Especialistas = $query->fetch_object();
+  ?>
+  
+  
+  
 
   
