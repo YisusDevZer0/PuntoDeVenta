dec4f93b4fd9f7deb2b79a6703291043ed5b0ff1
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-04-25 00:02:32 -0600

    Update RealizarCorte.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
@@ -5,7 +5,14 @@ include "../Controladores/ControladorUsuario.php";
 $user_id = null;
 $sql1 = "SELECT Venta_POS_ID,Folio_Ticket,Fk_Caja,Fk_sucursal FROM Ventas_POS WHERE Fk_Caja = " . $_POST["id"];
 $query = $conn->query($sql1);
-$Especialistas = $query->fetch_object();
+$Especialistas = null;
+if($query->num_rows>0){
+while ($r=$query->fetch_object()){
+  $Especialistas=$r;
+  break;
+}
+
+  }
 ?>
 
 <?php if ($Especialistas) : ?>
