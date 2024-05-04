56cbd4375a3a7b089d2a9e525eff5707b154ef99
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-04 06:44:49 -0600

    Personal

---
+++ PuntoDeVenta/RecursosHumanos/PersonalActivo.php
@@ -0,0 +1,109 @@
+
+<?php
+include_once "Controladores/ControladorUsuario.php";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>Personal Vigente <?php echo $row['Licencia']?></title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+   
+
+    <?php
+   include "header.php";?>
+   <div id="loading-overlay">
+  <div class="loader"></div>
+  <div id="loading-text" style="color: white; margin-top: 10px; font-size: 18px;"></div>
+</div>
+<body>
+    
+        <!-- Spinner End -->
+
+
+        <?php include_once "Menu.php" ?>
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+        <?php include "navbar.php";?>
+            <!-- Navbar End -->
+
+
+            <!-- Table Start -->
+          <div class="text-center"> 
+            <div class="container-fluid pt-4 px-4">
+    <div class="col-12">
+        <div class="bg-light rounded h-100 p-4">
+            <h6 class="mb-4" style="color:#0172b6;">Personal activo <?php echo $row['Licencia']?></h6>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+  Agregar nuevo personal
+</button> <br>
+            <div id="DataDeServicios"></div>
+            </div></div></div></div>
+            </div>
+          
+<script src="js/ControlDeUsuarios.js"></script>
+<script src="js/AgregarUsuarioss.js"></script>
+
+            <!-- Footer Start -->
+            <?php 
+            include "Modales/AltaNuevoUsuario.php";
+           
+            include "Modales/Modales_Referencias.php";
+            include "Footer.php";?>
+
+<script>
+   $(document).ready(function() {
+    // Delegación de eventos para el botón "btn-Movimientos" dentro de .dropdown-menu
+    $(document).on("click", ".btn-edita", function() {
+      console.log("Botón de cancelar clickeado para el ID:", id);
+        var id = $(this).data("id");
+        $.post("https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Modales/EditaTipoDeUsuario.php", { id: id }, function(data) {
+            $("#FormCajas").html(data);
+            $("#TitulosCajas").html("Editar servicios");
+            
+        });
+        $('#ModalEdDele').modal('show');
+    });
+
+    // Delegación de eventos para el botón "btn-Ventas" dentro de .dropdown-menu
+    $(document).on("click", ".btn-elimina", function() {
+        var id = $(this).data("id");
+        $.post("https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Modales/EliminarTipoUsuario.php", { id: id }, function(data) {
+            $("#FormCajas").html(data);
+            $("#TitulosCajas").html("Eliminar servicio");
+           
+        });
+        $('#ModalEdDele').modal('show');
+    });
+
+   
+});
+
+</script>
+
+  <div class="modal fade" id="ModalEdDele" tabindex="-1" role="dialog" style="overflow-y: scroll;" aria-labelledby="ModalEdDeleLabel" aria-hidden="true">
+  <div id="CajasDi"class="modal-dialog  modal-notify modal-success" >
+    <div class="text-center">
+      <div class="modal-content">
+      <div class="modal-header" style=" background-color: #ef7980 !important;" >
+         <p class="heading lead" id="TitulosCajas"  style="color:white;" ></p>
+
+         
+       </div>
+        
+	        <div class="modal-body">
+          <div class="text-center">
+        <div id="FormCajas"></div>
+        
+        </div>
+
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal --></div>
+</body>
+
+</html>
---
+++ PuntoDeVenta/RecursosHumanos/Personaldebaja.php
@@ -0,0 +1,55 @@
+<?php
+include_once "Controladores/ControladorUsuario.php";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>Personal inactivo <?php echo $row['Licencia']?></title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <?php
+   include "header.php";?>
+<body>
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <?php include_once "Menu.php" ?>
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+        <?php include "navbar.php";?>
+            <!-- Navbar End -->
+
+
+            <!-- Table Start -->
+          
+            <div class="container-fluid pt-4 px-4">
+    <div class="col-12">
+        <div class="bg-light rounded h-100 p-4">
+            <h6 class="mb-4">Personal Inactivo</h6>
+            <div id="tablaPersonalinactivo"></div>
+            </div></div></div></div>
+<script src="js/PersonalDebaja.js"></script>
+
+
+            </div>   </div>
+            <!-- Footer Start -->
+            <?php 
+           
+            include "Footer.php";?>
+</body>
+
+</html>
