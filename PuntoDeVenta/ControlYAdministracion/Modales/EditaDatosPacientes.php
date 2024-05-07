bc068686bcbe2cbd0b0a71e7af8e420a700635f2
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-06 22:09:23 -0600

    Update EditaDatosPacientes.php

--- PuntoDeVenta/ControlYAdministracion/Modales/EditaDatosPacientes.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/EditaDatosPacientes.php
@@ -44,7 +44,21 @@ while ($r=$query->fetch_object()){
      <div class="input-group mb-3">
   
   <input type="text" class="form-control "  id="actnomserv" name="ActNomServ" value="<?php echo $Especialistas->Nombre_Paciente; ?>" aria-describedby="basic-addon1" maxlength="60">            
-</div></div></div>
+</div></div>
+<div class="form-group">
+    <label for="exampleFormControlInput1">Fecha de nacimiento <span class="text-danger">*</span></label>
+     <div class="input-group mb-3">
+  
+  <input type="date" class="form-control "  id="actfechanac" name="ActFechaNac" value="<?php echo $Especialistas->Fecha_Nacimiento; ?>" aria-describedby="basic-addon1" maxlength="60">            
+</div></div>
+
+<div class="form-group">
+    <label for="exampleFormControlInput1">Edad <span class="text-danger">*</span></label>
+     <div class="input-group mb-3">
+  
+  <input type="text" class="form-control "  id="actedad" name="ActEdad" value="<?php echo $Especialistas->Edad; ?>" aria-describedby="basic-addon1" maxlength="60">            
+</div></div>
+</div>
 
     
 </div>
@@ -61,3 +75,26 @@ while ($r=$query->fetch_object()){
 <?php else:?>
   <p class="alert alert-danger">404 No se encuentra</p>
 <?php endif;?>
+<script>
+    // Función para calcular la edad
+    function calcularEdad() {
+        // Obtener la fecha de nacimiento del input
+        var fechaNacimiento = new Date(document.getElementById('actfechanac').value);
+        // Obtener la fecha actual
+        var fechaActual = new Date();
+        
+        // Calcular la diferencia en milisegundos
+        var diferencia = fechaActual - fechaNacimiento;
+        
+        // Calcular la edad en años
+        var edad = Math.floor(diferencia / (1000 * 60 * 60 * 24 * 365.25));
+        
+        // Actualizar el valor del input de edad
+        document.getElementById('actedad').value = edad;
+    }
+
+    // Llamar a la función calcularEdad cada vez que se abra el modal
+    $('#ModalEdDele').on('show.bs.modal', function (e) {
+        calcularEdad();
+    });
+</script>
