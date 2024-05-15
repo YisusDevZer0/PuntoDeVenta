c9d2d249b2c3822f62edf6e1aa79a57113f01c93
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-14 19:31:21 -0600

    Update IngresoDeMedicamentos.js

--- PuntoDeVenta/PuntoDeVentaFarmacias/js/IngresoDeMedicamentos.js
+++ PuntoDeVenta/PuntoDeVentaFarmacias/js/IngresoDeMedicamentos.js
@@ -2,7 +2,7 @@ function IngresosProductos(){
 
 
     $.get("https://doctorpez.mx/PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/IngresosProductos.php","",function(data){
-      $("#TableStockSucursales").html(data);
+      $("#DataDeProductos").html(data);
     })
   
   }
