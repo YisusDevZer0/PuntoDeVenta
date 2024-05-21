55e4a77429a1c51e97399a5faa54ff0347767146
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-20 21:14:49 -0600

    Update ArrayDesgloseRecordatoriosMensajes.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayDesgloseRecordatoriosMensajes.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayDesgloseRecordatoriosMensajes.php
@@ -54,7 +54,7 @@ while ($fila = $result->fetch_assoc()) {
         "Estatus" => $fila["Estado"],
         
         "Editar" => "<a href='https://saludapos.com/AdminPOS/CoincidenciaSucursales?Disid=" . base64_encode($fila["ID_Prod_POS"]) . "' type='button' class='btn btn-info btn-sm'><i class='fas fa-capsules'></i></a>",
-        "Eliminar" => "<a href='https://saludapos.com/AdminPOS/ActualizaOne?idProd=" . base64_encode($fila["Folio_Prod_Stock"]) . "' type='button' class='btn btn-info btn-sm'><i class='fas fa-capsules'></i></a>",
+        
     ];
 }
 
