89bed2bc74a124fcbcbb4c85af9e0379921352e7
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-21 21:32:03 -0600

    Update NuevoRecordatorio.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/NuevoRecordatorio.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/NuevoRecordatorio.php
@@ -2,8 +2,8 @@
 <div class="modal fade" id="ModalRecordatoriosMensajes" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Dejar un nuevo mensaje o recordatorio</h5>
+    <div class="modal-header" style=" background-color: #ef7980 !important;">
+        <h5 class="modal-title" style="color:white;">Dejar un nuevo mensaje o recordatorio</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
