b29224271a12c5f107ebfa633e71c357404495f9
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-21 22:18:27 -0600

    Update navbar.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/navbar.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/navbar.php
@@ -1,3 +1,8 @@
+<?php
+// Asegúrate de que $Licencia y $Fk_Sucursal están definidos y tienen valores
+$Licencia = isset($row['Licencia']) ? $row['Licencia'] : '';
+$Fk_Sucursal = isset($row['Fk_Sucursal']) ? $row['Fk_Sucursal'] : '';?>
+
 <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
                 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
