b8011f0a5a7b86ac5e1a7e8cd874fa2d7c1b7c8d
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-23 21:28:11 -0600

    Update RegistroEnergiaModal.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RegistroEnergiaModal.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RegistroEnergiaModal.php
@@ -1,89 +1,40 @@
-
-<div class="modal fade" id="RegistroEnergiaVentanaModal" tabindex="-1" role="dialog" style="overflow-y: scroll;" aria-labelledby="editModalLabel" aria-hidden="true">
-  <div id="Di"class="modal-dialog  modal-notify modal-success">
-      <div class="modal-content">
+<!-- Modal -->
+<div class="modal fade" id="RegistroEnergiaVentanaModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
       <div class="modal-header">
-         <p class="heading lead" id="Titulo">Control de energia</p>
-
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-hidden="true" class="white-text">&times;</span>
-         </button>
-       </div>
-        <div id="Mensaje "class="alert alert-info alert-styled-left text-blue-800 content-group">
-						                <span id="Aviso" class="text-semibold">Estimado usuario, 
-                            Verifique los campos antes de realizar alguna accion</span>
-						                <button type="button" class="close" data-dismiss="alert">×</button>
-                            </div>
-	        <div class="modal-body">
-          <div class="text-center">
-          <form enctype="multipart/form-data" id="RegistroDiarioEnergiaWats">
-<div class="form-group">
-    <label for="exampleFormControlInput1">Ingrese el valor de kilowatts </label>
-     <div class="input-group mb-3">
-  <div class="input-group-prepend">
-  <span class="input-group-text" id="Tarjeta"><i class="fas fa-lightbulb"></i></span>
-  </div>
-  <input type="number" class="form-control"   name="RegistroEnergia" id="registroenergia" aria-describedby="basic-addon1" required>
-  <input type="text"  hidden name="Fecha" value="<?php echo date('Y-m-d'); ?> ">
-</div>
-<div>
-</div>
-    </div>
-    <div class="form-group">
-    <label for="exampleFormControlInput1">Comentario</label>
-     <div class="input-group mb-3">
-  <div class="input-group-prepend">
-  <span class="input-group-text" id="Tarjeta"><i class="fas fa-comment"></i></span>
-  </div>
-  <textarea class="form-control" id="Comentario" name="Comentario" rows="3"></textarea>
-</div>
-
-    </div>
-    <div class="form-group">
-    <label for="exampleFormControlInput1">Fotografia</label>
-     <div class="input-group mb-3">
-  <div class="input-group-prepend">
-  <span class="input-group-text" id="Tarjeta"><i class="fas fa-comment"></i></span>
-  </div>
-  <input type="file" required class="form-control " name="file" id="file" aria-describedby="basic-addon1" >          
-</div>
-<div>
-
-</div>
-    </div>
+        <h5 class="modal-title" id="exampleModalLabel">Agregar Nueva presentacion</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Formulario con el estilo proporcionado -->
+        <div class="row">
+          <div class="col-12">
+            <div class="bg-light rounded p-4">
+              
+              <form id="NewTypeUser">
+                  <!-- Agrega un campo oculto para el token CSRF -->
     
-    </div>
-   
-    
- 
-  
-<!-- INICIA DATA DE AGENDA -->
+                <div class="mb-3">
+                  <label for="tipouser" class="form-label">Nombre de la presentacion</label>
+                  <input type="text" class="form-control" name="NomPresentacion" id="nombreservicio" placeholder="Ingrese el nombre del servicio">
+                </div>
+                
+               
+                
+                <input type="text" hidden class="form-control" name="licencia" id ="licencia" value="<?php echo $row['Licencia']?>">
+                <input type="text" hidden class="form-control" name="estado" id ="estado" value="Vigente">
+                <input type="text" hidden class="form-control" name="agrego"id="agrego" value="<?php echo $row['Nombre_Apellidos']?>" >
+                <input type="text" hidden class="form-control" name="Sistema"id="sistema" value="<?php echo $row['Tipo_Usuario']?>" >
+                               <!-- Agrega los otros campos del formulario de manera similar -->
+                <!-- ... -->
 
-
-   
-    
-    
-     
-    <input type="text" class="form-control" name="Registro" id="registro" hidden value="<?php echo $row['Nombre_Apellidos']?>"  readonly >
-  
-    <input type="text" class="form-control" name="Sucursal" id="sucursal"  hidden value="<?php echo $row['Nombre_Sucursal']?>"   readonly >
-    <input type="text" class="form-control" name="Empresa" id="Empresa" hidden value="Doctor Consulta"   readonly >
-    <div class="text-center">
-<button type="submit"  name="submit_AgeExt" id="submit_AgeExt"  class="btn btn-success">Confirmar datos <i class="fas fa-user-check"></i></button>
-    </div>    </div></div>
-<!-- FINALIZA DATA DE AGENDA -->
-                  
-</form>
-
-
-</div></div>
-
-
-
-
-        
+                <button type="submit" class="btn btn-primary">Guardar</button>
+              </form>
+            </div>
+          </div>
         </div>
-
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+      </div>
+    </div>
+  </div>
+</div>
