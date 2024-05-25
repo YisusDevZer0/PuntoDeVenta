e19c93aa8c765c98a12a565ca65e3d93f85e1197
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-25 09:56:45 -0600

    Update index.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/index.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/index.php
@@ -65,9 +65,7 @@ include_once "Controladores/ControladorUsuario.php"
         <div class="bg-light rounded h-100 p-4">
             <h6 class="mb-4" style="color:#0172b6;">Mensajes o recordatorios de  <?php echo $row['Licencia']?> Sucursal <?php echo $row['Nombre_Sucursal']?></h6>
             <div class="text-center">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalRecordatoriosMensajes">
-  Crear nuevo mensaje o recordatorio
-</button> <br>
+            
 <div id="Cajas"></div>
             </div></div></div></div>
                 </div>
