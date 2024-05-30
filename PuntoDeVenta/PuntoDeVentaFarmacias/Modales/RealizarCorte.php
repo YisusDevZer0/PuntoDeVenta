32515b11584727319f79d20ba6b1f626d956fcf2
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-05-29 20:26:12 -0600

    Update RealizarCorte.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/RealizarCorte.php
@@ -16,7 +16,36 @@ while ($r=$query->fetch_object()){
 ?>
 
 <?php if ($Especialistas) : ?>
-    
+  <?php if($query->num_rows>0):?>
+  <div class="text-center">
+ 
+  <div class="row">
+    <div class="col">
+  <label for="exampleFormControlInput1">Sucursal</label>
+  <input type="text" class="form-control "  id="cantidadtotalventasss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas14->Nombre_Sucursal; ?>" aria-describedby="basic-addon1" >
+  </div>
+  <div class="col">
+  <label for="exampleFormControlInput1">Turno</label>
+  <input type="text" class="form-control "  id="cantidadtotalventasss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas3->Turno; ?>" aria-describedby="basic-addon1" >
+  </div>  </div>
+  <div class="row">
+    <div class="col">
+  <label for="exampleFormControlInput1">Cajero</label>
+  <input type="text" class="form-control "  id="cantidadtotalventassss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas3->AgregadoPor; ?>" aria-describedby="basic-addon1" >
+  </div> 
+  <div class="col">
+  <label for="exampleFormControlInput1">Total de venta</label>
+  <input type="number" class="form-control "  id="cantidadtotalventassss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas3->VentaTotal; ?>" aria-describedby="basic-addon1" > 
+  </div>  </div>
+  <div class="row">
+    <div class="col">
+  <label for="exampleFormControlInput1">Total de tickets</label>
+  <input type="text" class="form-control "  id="cantidadtotalventassss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas3->Total_tickets; ?>" aria-describedby="basic-addon1" >
+  </div> 
+  <div class="col">
+  <label for="exampleFormControlInput1">Total de signos vitales</label>
+  <input type="number" class="form-control "  id="cantidadtotalventasssss" name="TicketVentasTotl" step="any" readonly value="<?php echo $Especialistas3->Total_Folios; ?>" aria-describedby="basic-addon1" > 
+  </div>  </div>
 <?php else : ?>
     <p class="alert alert-danger">404 No se encuentra</p>
 <?php endif; ?>
