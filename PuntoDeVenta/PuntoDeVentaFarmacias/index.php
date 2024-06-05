73edabf512f7289b92351fc3ee8b7444240d5493
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-04 20:35:36 -0600

    Update index.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/index.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/index.php
@@ -12,9 +12,7 @@ include_once "Controladores/ControladorUsuario.php";
     <meta content="" name="description">
 
     <!-- Incluir dependencias de Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
 
     <?php
     include "header.php";
