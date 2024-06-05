ddf3982e548a641ea0fdf5d2ec3017f64399c743
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-04 20:41:01 -0600

    Update Footer.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Footer.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Footer.php
@@ -24,3 +24,14 @@
 
     <!-- JavaScript Libraries -->
   
+
+    <script src="lib/chart/chart.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
