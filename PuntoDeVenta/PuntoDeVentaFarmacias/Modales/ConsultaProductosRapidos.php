41f84a4f538fdd721efb70bdf994bdd24f0fca39
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-04 20:35:15 -0600

    xd

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/ConsultaProductosRapidos.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/ConsultaProductosRapidos.php
@@ -1,32 +1,41 @@
-
-
-<!-- Central Modal Medium Info -->
-<div class="modal fade" id="ConsultaProductos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-   aria-="true" style="overflow-y: scroll;">
-   <div class="modal-dialog modal-xl modal-notify modal-success" role="document">
-     <!--Content-->
-     <div class="modal-content">
-       <!--Header-->
-       <div class="modal-header">
-         <p class="heading lead">Consulta de producto</p>
-
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-="true" class="white-text">&times;</span>
-         </button>
-       </div>
-     
-       <!--Body-->
-       <div class="modal-body">
-         <div class="text-center">
-    
-                                      
-         
-       </div>
-     </div>
-     <!--/.Content-->
-   </div>
- </div>
- </div>
-
-
- 
+  <!-- Modal -->
+  <div class="modal fade" id="ConsultaProductos" tabindex="-1" role="dialog" aria-labelledby="resultModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-notify modal-success" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="resultModalLabel">Consulta de producto</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="text-center">
+                            <!-- Aquí se mostrarán los detalles del producto -->
+                            <table class="table table-bordered" id="detailTable">
+                                <thead>
+                                    <tr>
+                                        <th>Código de Barra</th>
+                                        <th>Clave Adicional</th>
+                                        <th>Nombre del Producto</th>
+                                        <th>Precio de Venta</th>
+                                        <th>Servicio</th>
+                                        <th>Tipo</th>
+                                        <th>Proveedor 1</th>
+                                        <th>Proveedor 2</th>
+                                        <th>Último Movimiento</th>
+                                        <th>Existencias</th>
+                                        <th>Min. Existencia</th>
+                                        <th>Max. Existencia</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr id="detailRow">
+                                        <!-- Detalles del producto -->
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
--- PuntoDeVenta/PuntoDeVentaFarmacias/index.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/index.php
@@ -78,47 +78,7 @@ include_once "Controladores/ControladorUsuario.php";
             </div>
         </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="ConsultaProductos" tabindex="-1" role="dialog" aria-labelledby="resultModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl modal-notify modal-success" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="resultModalLabel">Consulta de producto</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="text-center">
-                            <!-- Aquí se mostrarán los detalles del producto -->
-                            <table class="table table-bordered" id="detailTable">
-                                <thead>
-                                    <tr>
-                                        <th>Código de Barra</th>
-                                        <th>Clave Adicional</th>
-                                        <th>Nombre del Producto</th>
-                                        <th>Precio de Venta</th>
-                                        <th>Servicio</th>
-                                        <th>Tipo</th>
-                                        <th>Proveedor 1</th>
-                                        <th>Proveedor 2</th>
-                                        <th>Último Movimiento</th>
-                                        <th>Existencias</th>
-                                        <th>Min. Existencia</th>
-                                        <th>Max. Existencia</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr id="detailRow">
-                                        <!-- Detalles del producto -->
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <?php include "Modales/ConsultaProductosRapidos.php"?>
 
         <script src="js/Recordatorios_mensajes.js"></script>
 
