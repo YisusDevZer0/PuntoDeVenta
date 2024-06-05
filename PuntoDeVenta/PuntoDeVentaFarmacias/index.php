afb5a06d7723c7fa7d2974af3eb12dc47c24ea6e
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-04 20:36:03 -0600

    Update index.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/index.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once "Controladores/ControladorUsuario.php";
+include_once "Controladores/ControladorUsuario.php"
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -11,13 +11,8 @@ include_once "Controladores/ControladorUsuario.php";
     <meta content="" name="keywords">
     <meta content="" name="description">
 
-    <!-- Incluir dependencias de Bootstrap -->
-
-
-    <?php
-    include "header.php";
-    ?>
-</head>
+   <?php
+   include "header.php";?>
 
 <body>
     <div class="container-xxl position-relative bg-white d-flex p-0">
@@ -29,15 +24,18 @@ include_once "Controladores/ControladorUsuario.php";
         </div>
         <!-- Spinner End -->
 
+
         <!-- Sidebar Start -->
-        <?php include_once "Menu.php"; ?>
+       <?php include_once "Menu.php" ?>
         <!-- Sidebar End -->
+
 
         <!-- Content Start -->
         <div class="content">
             <!-- Navbar Start -->
-            <?php include "navbar.php"; ?>
+            <?php include "navbar.php";?>
             <!-- Navbar End -->
+
 
             <!-- Sale & Revenue Start -->
             <div class="container-fluid pt-4 px-4">
@@ -48,11 +46,13 @@ include_once "Controladores/ControladorUsuario.php";
                             <div class="ms-3">
                                 <p class="mb-2">Productos</p>
                                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ConsultaProductos">Consultar</button>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-6 col-xl-3">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                           
                             <i class="fa-solid fa-right-left fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2">Traspasos</p>
@@ -60,30 +60,24 @@ include_once "Controladores/ControladorUsuario.php";
                             </div>
                         </div>
                     </div>
-                </div>
+                   
+                    <div class="container-fluid pt-4 px-8">
+    <div class="col-12">
+        <div class="bg-light rounded h-100 p-4">
+            <h6 class="mb-4" style="color:#0172b6;">Mensajes o recordatorios de  <?php echo $row['Licencia']?> Sucursal <?php echo $row['Nombre_Sucursal']?></h6>
+            <div class="text-center">
+            
+<div id="Cajas"></div>
+            </div></div></div></div>
+            <!-- Sale & Revenue End -->
 
-                <div class="container-fluid pt-4 px-8">
-                    <div class="col-12">
-                        <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4" style="color:#0172b6;">Mensajes o recordatorios de <?php echo $row['Licencia'] ?> Sucursal <?php echo $row['Nombre_Sucursal'] ?></h6>
-                            <div class="text-center">
-                                <div id="Cajas"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Sale & Revenue End -->
-            </div>
-        </div>
+            <script src="js/Recordatorios_mensajes.js"></script>
+       
 
-      <?php include "Modales/ConsultaProductosRapidos.php"?>
 
-        <script src="js/Recordatorios_mensajes.js"></script>
-
-        <?php
-        include "Footer.php";
-        ?>
-    </div>
+        <?php 
+        include "Modales/ConsultaProductosRapidos.php";
+        include "Footer.php";?>
 </body>
 
 </html>
