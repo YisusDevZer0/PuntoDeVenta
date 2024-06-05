7f2dae30fc6b915414f3610e7e3a50f8df59739d
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-04 20:37:30 -0600

    Update index.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/index.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/index.php
@@ -11,18 +11,15 @@ include_once "Controladores/ControladorUsuario.php"
     <meta content="" name="keywords">
     <meta content="" name="description">
 
+    <div id="loading-overlay">
+  <div class="loader"></div>
+  <div id="loading-text" style="color: white; margin-top: 10px; font-size: 18px;"></div>
+</div>
    <?php
    include "header.php";?>
 
 <body>
-    <div class="container-xxl position-relative bg-white d-flex p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
+  
 
 
         <!-- Sidebar Start -->
