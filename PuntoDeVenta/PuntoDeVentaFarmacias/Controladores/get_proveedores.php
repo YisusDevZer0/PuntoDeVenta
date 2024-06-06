45d62b27aac97b9f2a5072ab279a0f07115b2a52
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-05 21:14:04 -0600

    Update get_proveedores.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/get_proveedores.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/get_proveedores.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 include("db_connect.php");
 
 // Consulta para obtener los proveedores
-$sql = "SELECT DISTINCT Proveedor FROM Proveedores"; // Ajusta el nombre de la tabla y la columna según tu estructura
+$sql = "SELECT DISTINCT Nombre_Proveedor FROM Proveedores"; // Ajusta el nombre de la tabla y la columna según tu estructura
 
 // Ejecutar la consulta
 $result = $conn->query($sql);
