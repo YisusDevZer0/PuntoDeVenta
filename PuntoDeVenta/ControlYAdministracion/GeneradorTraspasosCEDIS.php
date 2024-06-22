ef27cd08d6b3c19cd8037bbf9a48b0ed3971f423
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-22 16:41:32 -0600

    Update GeneradorTraspasosCEDIS.php

--- PuntoDeVenta/ControlYAdministracion/GeneradorTraspasosCEDIS.php
+++ PuntoDeVenta/ControlYAdministracion/GeneradorTraspasosCEDIS.php
@@ -694,7 +694,7 @@ function buscarArticulo(codigoEscaneado) {
   if (!codigoEscaneado.trim()) return; // No hacer nada si el código está vacío
 
   $.ajax({
-    url: "Consultas/escaner_articulo.php",
+    url: "Controladores/BusquedaPorEscaner.php",
     type: 'POST',
     data: { codigoEscaneado: codigoEscaneado },
     dataType: 'json',
@@ -790,7 +790,7 @@ $('#codigoEscaneado').autocomplete({
   source: function (request, response) {
     // Realiza una solicitud AJAX para obtener los resultados de autocompletado
     $.ajax({
-      url: 'Consultas/autocompletado.php',
+      url: 'Controladores/DespliegaAutoComplete.php',
       type: 'GET',
       dataType: 'json',
       data: {
