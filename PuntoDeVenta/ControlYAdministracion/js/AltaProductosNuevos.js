f782d1511f259221857267ad927b74459790d114
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-22 20:45:03 -0600

    xd

--- PuntoDeVenta/ControlYAdministracion/js/AltaProductosNuevos.js
+++ PuntoDeVenta/ControlYAdministracion/js/AltaProductosNuevos.js
@@ -2,25 +2,27 @@ $('document').ready(function ($) {
   $.validator.addMethod("Sololetras", function (value, element) {
     return this.optional(element) || /[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]*$/.test(value);
   }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+
   $.validator.addMethod("Telefonico", function (value, element) {
     return this.optional(element) || /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/.test(value);
-  }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar numeros!");
+  }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar números!");
+
   $.validator.addMethod("Correos", function (value, element) {
     return this.optional(element) || /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(value);
-  }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Ingresa un correo valido!");
+  }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Ingresa un correo válido!");
+
   $.validator.addMethod("NEmpresa", function (value, element) {
     return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
   }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+
   $.validator.addMethod("Problema", function (value, element) {
     return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
   }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
 
-
   $("#AgregaProductos").validate({
     rules: {
-
-      CodBarra:{
-        required:true,
+      CodBarra: {
+        required: true,
       },
       Clav: {
         maxlength: 15,
@@ -36,39 +38,31 @@ $('document').ready(function ($) {
       PC: {
         required: true,
       },
-      
       MinE: {
         required: true,
       },
       MaxE: {
         required: true,
       },
-     
-      
-      ExistenciaCedis:
-      {
-        required:true,
+      ExistenciaCedis: {
+        required: true,
       },
-     
-      Provee1:{
-        required:true,
+      Provee1: {
+        required: true,
       },
-      tiposervicio:{
-        required:true,
+      tiposervicio: {
+        required: true,
       },
     },
     messages: {
-
       Clav: {
-       
-        maxlength: "No puedes ingresar mas de 5 caracteres",
-        minlength: "Debes ingresar minimo 3 caracteres"
-
+        maxlength: "No puedes ingresar más de 5 caracteres",
+        minlength: "Debes ingresar mínimo 3 caracteres"
       },
       NombreProd: {
         required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
-        maxlength: "No puedes ingresar mas de 5 caracteres",
-        minlength: "Debes ingresar minimo 3 caracteres"
+        maxlength: "No puedes ingresar más de 5 caracteres",
+        minlength: "Debes ingresar mínimo 3 caracteres"
       },
       Sucursal: {
         required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
@@ -85,96 +79,62 @@ $('document').ready(function ($) {
       MaxE: {
         required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
       },
-      
       Provee1: {
         required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
       },
-      tiposervicio:{
+      tiposervicio: {
         required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
       },
     },
     submitHandler: submitForm
   });
-  // hide messages 
-
 
   function submitForm() {
-
-
-
     $.ajax({
       type: 'POST',
       url: "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/AltaProductos.php",
       data: $('#AgregaProductos').serialize(),
       cache: false,
       beforeSend: function () {
-
         $("#submit_registro").html("Verificando datos... <span class='fa fa-refresh fa-spin' role='status' aria-hidden='true'></span>");
-
-
       },
       success: function (dataResult) {
         var dataResult = JSON.parse(dataResult);
 
         if (dataResult.statusCode == 250) {
-          var modal_lv = 0;
-          $('.modal').on('shown.bs.modal', function (e) {
-            $('.modal-backdrop:last').css('zIndex', 1051 + modal_lv);
-            $(e.currentTarget).css('zIndex', 1052 + modal_lv);
-            modal_lv++
+          swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Algo salió mal...',
           });
-
-          $('.modal').on('hidden.bs.modal', function (e) {
-            modal_lv--
+          $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
+        } else if (dataResult.statusCode == 200) {
+          swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'Registro exitoso!',
+          }).then((result) => {
+            if (result.isConfirmed || result.isDismissed) {
+              location.reload(); // Recargar la página
+            }
           });
-          $("#submit_registro").html("Algo no salio bien.. <i class='fas fa-exclamation-triangle'></i>");
-          $('#ErrorDupli').modal('toggle');
-          setTimeout(function () {
-          }, 2000); // abrir
-          setTimeout(function () {
-            $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
-          }, 3000); // abrir
-
-
+                   $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
+        } else if (dataResult.statusCode == 201) {
+          swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Algo salió mal...',
+          });
+          $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
         }
-
-        else if (dataResult.statusCode == 200) {
-
-          $("#submit_registro").html("Enviado <i class='fas fa-check'></i>")
-
-          $("#AgregaProductos")[0].reset();
-          $("myModal").removeClass("in");
-          $(".modal-backdrop").remove();
-          $("myModal").hide();
-        
-
-          $('#AltaCorrecta').modal('togglesuccessModal');
-          setTimeout(function () {
-            $('#AltaCorrecta').modal('hide')
-            location.reload(); // Recargar la página completa después de 5 segundos
-          }, 4000); // abrir
-          
-
-
-          //  Solucionar muestra de modal de AltaCorrecta
-
-
-        }
-        else if (dataResult.statusCode == 201) {
-          $("#submit_Age").html("Algo no salio bien.. <i class='fas fa-exclamation-triangle'></i>");
-          $('#ErrorData').modal('toggle');
-
-          setTimeout(function () {
-            $("#submit_Age").html("Guardar <i class='fas fa-save'></i>");
-          }, 3000); // abrir
-
-
-        }
-
-
-
-
-
+      },
+      error: function () {
+        swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Error de conexión...',
+        });
+        $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
       }
     });
     return false;
