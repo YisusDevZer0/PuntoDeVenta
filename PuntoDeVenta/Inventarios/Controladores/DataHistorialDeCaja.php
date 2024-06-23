08f4cc5ee875150e413bee031e6fe9cf581fabed
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-22 21:08:00 -0600

    Update DataHistorialDeCaja.php

--- PuntoDeVenta/Inventarios/Controladores/DataHistorialDeCaja.php
+++ PuntoDeVenta/Inventarios/Controladores/DataHistorialDeCaja.php
@@ -208,7 +208,7 @@ tabla = $('#Clientes').DataTable({
  "stateSave":true,
  "bAutoWidth": false,
  "order": [[ 0, "desc" ]],
- "sAjaxSource": "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/ArrayDeCajasCerradas.php",
+ "sAjaxSource": "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/ArrayHistorialCajas.php",
  "aoColumns": [
   { mData: 'IdCaja' },  
   { mData: 'Empleado' },
