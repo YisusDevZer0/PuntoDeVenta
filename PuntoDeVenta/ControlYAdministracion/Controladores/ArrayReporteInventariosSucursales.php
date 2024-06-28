ce0a7310a01b7099f580a1bf0851f0ea94d401a3
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-27 22:25:20 -0600

    Update ArrayReporteInventariosSucursales.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/ArrayReporteInventariosSucursales.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/ArrayReporteInventariosSucursales.php
@@ -5,26 +5,26 @@ include_once "ControladorUsuario.php";
 
 // Consulta segura utilizando una sentencia preparada
 $sql = "SELECT 
-    ci.`IdProdCedis`, 
-    ci.`ID_Prod_POS`, 
-    ci.`Cod_Barra`, 
-    ci.`Nombre_Prod`, 
-    ci.`Contabilizado`, 
-    ci.`StockEnMomento`, 
-    ci.`ExistenciasAjuste`, 
-    ci.`Precio_Venta`, 
-    ci.`Precio_C`, 
-    ci.`AgregadoPor`, 
-    ci.`AgregadoEl`, 
-    ci.`FechaInventario`,
-    ci.`Fk_Sucursal`,
+    is.`IdProdCedis`, 
+    is.`ID_Prod_POS`, 
+    is.`Cod_Barra`, 
+    is.`Nombre_Prod`, 
+    is.`Contabilizado`, 
+    is.`StockEnMomento`, 
+    is.`ExistenciasAjuste`, 
+    is.`Precio_Venta`, 
+    is.`Precio_C`, 
+    is.`AgregadoPor`, 
+    is.`AgregadoEl`, 
+    is.`FechaInventario`,
+    is.`Fk_Sucursal`,
     s.`Nombre_Sucursal`,
-    (ci.`Contabilizado` * ci.`Precio_Venta`) AS `Total_Precio_Venta`,
-    (ci.`Contabilizado` * ci.`Precio_C`) AS `Total_Precio_Compra`
+    (is.`Contabilizado` * is.`Precio_Venta`) AS `Total_Precio_Venta`,
+    (is.`Contabilizado` * is.`Precio_C`) AS `Total_Precio_Compra`
 FROM 
-    `Cedis_Inventarios` ci
+    `InventariosSucursales` is
 LEFT JOIN 
-    `Sucursales` s ON ci.`Fk_Sucursal` = s.`ID_Sucursal`";
+    `Sucursales` s ON is.`Fk_Sucursal` = s.`ID_Sucursal`";
 
 // Preparar la declaración
 $stmt = $conn->prepare($sql);
@@ -43,7 +43,6 @@ while ($fila = $result->fetch_assoc()) {
     // Construir el array de datos
     $data[] = [
         "ID_Traspaso_Generado" => $fila["IdProdCedis"], // Assuming "IdProdCedis" corresponds to "ID_Traspaso_Generado"
-      
         "Cod_Barra" => $fila["Cod_Barra"],
         "Nombre_Prod" => $fila["Nombre_Prod"],
         "Precio_Venta" => $fila["Precio_Venta"],
@@ -52,7 +51,6 @@ while ($fila = $result->fetch_assoc()) {
         "Existencia" => $fila["StockEnMomento"], // Assuming "StockEnMomento" corresponds to "Existencia"
         "Diferencia" => $fila["ExistenciasAjuste"], // Assuming "ExistenciasAjuste" corresponds to "Diferencia"
         "FechaEntrega" => $fila["FechaInventario"], // Assuming "FechaInventario" corresponds to "FechaEntrega"
-       
         "AgregadoPor" => $fila["AgregadoPor"],
         "AgregadoEl" => $fila["AgregadoEl"],
         "Nombre_Sucursal" => $fila["Nombre_Sucursal"] // Adding the sucursal name to the result
