404164b178fd0eb5d6e95c92e8d982454aa6e364
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-17 21:53:15 -0600

    Update SolicitudPendientes.php

--- PuntoDeVenta/ControlYAdministracion/SolicitudPendientes.php
+++ PuntoDeVenta/ControlYAdministracion/SolicitudPendientes.php
@@ -8,7 +8,7 @@ include_once "Controladores/ControladorUsuario.php";
 
 <head>
     <meta charset="utf-8">
-    <title>Administracion de cajas de <?php echo $row['Licencia']?></title>
+    <title>Solicitudes de ingresos pendientes <?php echo $row['Licencia']?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
    
     <div id="loading-overlay">
