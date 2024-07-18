b339e3c86d8e619ca45e41dcc0545b1dd679da21
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-17 21:53:58 -0600

    Update SolicitudesPendientes.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/SolicitudesPendientes.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/SolicitudesPendientes.php
@@ -264,7 +264,7 @@ tabla = $('#Clientes').DataTable({
 //   }
 // ],
 // Personalizar la posición de los elementos del encabezado
-"dom": '<"d-flex justify-content-between"lBf>rtip', // Modificar la disposición aquí
+"dom": '<"d-flex justify-content-between">rtip', // Modificar la disposición aquí
 "responsive": true
 });
 </script>
