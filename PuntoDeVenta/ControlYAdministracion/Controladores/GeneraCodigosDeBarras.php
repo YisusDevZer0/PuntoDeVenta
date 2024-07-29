dfa543fc79fafa98f9a4a197e42e2911aeab64ff
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-28 18:45:22 -0600

    Update GeneraCodigosDeBarras.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/GeneraCodigosDeBarras.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/GeneraCodigosDeBarras.php
@@ -1,19 +1,20 @@
 <?php
 include "db_connect.php";
 
-        $idProdPos=$conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['ID_Prod_POSAct']))));
-       $codBarraActualiza=$conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Cod_BarraActualiza']))));
-        
+// Sanitizar y validar las entradas
+$idProdPos = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['ID_Prod_POSAct']))));
+$codBarraActualiza = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['Cod_BarraActualiza']))));
 
-        $sql = "UPDATE `Productos_POS` 
-        SET `codBarraActualiza`='$codBarraActualiza'
-        WHERE idProdPos=$idProdPos";
-       if (mysqli_query($conn, $sql)) {
-		echo json_encode(array("statusCode"=>200));
-	} 
-	else {
-		echo json_encode(array("statusCode"=>201));
-	}
-	mysqli_close($conn);
+// Consulta SQL con comillas simples para valores de cadena
+$sql = "UPDATE `Productos_POS` 
+        SET `codBarraActualiza` = '$codBarraActualiza'
+        WHERE `idProdPos` = $idProdPos";
 
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array("statusCode" => 200));
+} else {
+    echo json_encode(array("statusCode" => 500, "error" => mysqli_error($conn)));
+}
+
+mysqli_close($conn);
 ?>
