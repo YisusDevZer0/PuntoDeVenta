ee78bb89ee85fd56d29f5769fa0ba2b8a951dba9
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-28 18:47:01 -0600

    Update GeneraCodigosDeBarras.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/GeneraCodigosDeBarras.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/GeneraCodigosDeBarras.php
@@ -1,11 +1,9 @@
 <?php
 include "db_connect.php";
 
-// Sanitizar y validar las entradas
 $idProdPos = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['ID_Prod_POSAct']))));
 $codBarraActualiza = $conn->real_escape_string(htmlentities(strip_tags(trim($_POST['Cod_BarraActualiza']))));
 
-// Consulta SQL con comillas simples para valores de cadena
 $sql = "UPDATE `Productos_POS` 
         SET `codBarraActualiza` = '$codBarraActualiza'
         WHERE `idProdPos` = $idProdPos";
