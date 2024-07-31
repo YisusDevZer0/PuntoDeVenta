15e73fb39df3cf221d42c9b16ce127499adbc73f
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-30 23:04:04 -0600

    Update DescargaDeInventariosSucursales.php

--- PuntoDeVenta/ControlYAdministracion/Modales/DescargaDeInventariosSucursales.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/DescargaDeInventariosSucursales.php
@@ -2,8 +2,8 @@
 <div class="modal fade" id="DescargaReporteInventarios" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Agregar Nuevo servicio</h5>
+    <div class="modal-header" style=" background-color: #ef7980 !important;">
+    <h5 class="modal-title" style="color:white;" id="exampleModalLabel">Generar reporte de inventarios</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
