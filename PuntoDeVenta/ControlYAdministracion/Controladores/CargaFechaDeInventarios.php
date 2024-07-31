56c9555e00496090e9ed7a944be9f47402615d67
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-07-30 23:00:12 -0600

    Update CargaFechaDeInventarios.php

--- PuntoDeVenta/ControlYAdministracion/Controladores/CargaFechaDeInventarios.php
+++ PuntoDeVenta/ControlYAdministracion/Controladores/CargaFechaDeInventarios.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 include("db_connect.php");
 
 // Consulta SQL para obtener las fechas
-$sql = "SELECT DISTINCT FechaInventario FROM InventariosStocks_Conteos;"; // Reemplaza 'InventariosStocks_Conteos' con el nombre de tu tabla
+$sql = "SELECT DISTINCT FechaInventario FROM InventariosSucursales;"; // Reemplaza 'InventariosStocks_Conteos' con el nombre de tu tabla
 $result = $conn->query($sql);
 
 $fechas = array();
