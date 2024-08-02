4290be306854b421c7762ea58916f18d5b756b2a
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-01 23:09:19 -0600

    Update ActualizacionMasiva.php

--- PuntoDeVenta/ControlYAdministracion/ActualizacionMasiva.php
+++ PuntoDeVenta/ControlYAdministracion/ActualizacionMasiva.php
@@ -9,10 +9,9 @@ if (isset($_POST["import"])) {
     if (in_array($_FILES["file"]["type"], $allowedFileType)) {
         $targetPath = 'subidas/' . $_FILES['file']['name'];
         move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
-        $Sucursal = mysqli_real_escape_string($con, $_POST["Sucursalnueva"]);
-        $Tipo_ajuste = mysqli_real_escape_string($con, $_POST["TipAjuste"]);
-        $Agrego = mysqli_real_escape_string($con, $_POST["Agrega"]);
-        $ActualizadoPor = mysqli_real_escape_string($con, $_POST["ActualizadoPor"]); // Hidden input for user
+      
+       
+        $ActualizadoPor = $row['Nombre_Apellidos']; // Hidden input for user
         $Reader = new SpreadsheetReader($targetPath);
         
         $sheetCount = count($Reader->sheets());
@@ -20,24 +19,25 @@ if (isset($_POST["import"])) {
             $Reader->ChangeSheet($i);
             
             foreach ($Reader as $Row) {
-                $Cod_Barra = isset($Row[0]) ? mysqli_real_escape_string($con, $Row[0]) : "";
-                $Clave_adicional = isset($Row[1]) ? mysqli_real_escape_string($con, $Row[1]) : "";
-                $Nombre_Prod = isset($Row[2]) ? mysqli_real_escape_string($con, $Row[2]) : "";
-                $Precio_Venta = isset($Row[3]) ? mysqli_real_escape_string($con, $Row[3]) : "";
-                $Precio_C = isset($Row[4]) ? mysqli_real_escape_string($con, $Row[4]) : "";
-                $Componente_Activo = isset($Row[5]) ? mysqli_real_escape_string($con, $Row[5]) : "";
-                $Tipo = isset($Row[6]) ? mysqli_real_escape_string($con, $Row[6]) : "";
-                $FkCategoria = isset($Row[7]) ? mysqli_real_escape_string($con, $Row[7]) : "";
-                $FkMarca = isset($Row[8]) ? mysqli_real_escape_string($con, $Row[8]) : "";
-                $FkPresentacion = isset($Row[9]) ? mysqli_real_escape_string($con, $Row[9]) : "";
-                $Proveedor1 = isset($Row[10]) ? mysqli_real_escape_string($con, $Row[10]) : "";
-                $Proveedor2 = isset($Row[11]) ? mysqli_real_escape_string($con, $Row[11]) : "";
-                $RecetaMedica = isset($Row[12]) ? mysqli_real_escape_string($con, $Row[12]) : "";
-                $Licencia = isset($Row[13]) ? mysqli_real_escape_string($con, $Row[13]) : "";
-                $Ivaal16 = isset($Row[14]) ? mysqli_real_escape_string($con, $Row[14]) : "";
+                $Id_Actualizado= isset($Row[0]) ? mysqli_real_escape_string($con, $Row[0]) : "";
+                $Cod_Barra = isset($Row[1]) ? mysqli_real_escape_string($con, $Row[1]) : "";
+                $Clave_adicional = isset($Row[2]) ? mysqli_real_escape_string($con, $Row[2]) : "";
+                $Nombre_Prod = isset($Row[3]) ? mysqli_real_escape_string($con, $Row[3]) : "";
+                $Precio_Venta = isset($Row[4]) ? mysqli_real_escape_string($con, $Row[4]) : "";
+                $Precio_C = isset($Row[5]) ? mysqli_real_escape_string($con, $Row[5]) : "";
+                $Componente_Activo = isset($Row[6]) ? mysqli_real_escape_string($con, $Row[6]) : "";
+                $Tipo = isset($Row[7]) ? mysqli_real_escape_string($con, $Row[7]) : "";
+                $FkCategoria = isset($Row[8]) ? mysqli_real_escape_string($con, $Row[8]) : "";
+                $FkMarca = isset($Row[9]) ? mysqli_real_escape_string($con, $Row[9]) : "";
+                $FkPresentacion = isset($Row[10]) ? mysqli_real_escape_string($con, $Row[10]) : "";
+                $Proveedor1 = isset($Row[11]) ? mysqli_real_escape_string($con, $Row[11]) : "";
+                $Proveedor2 = isset($Row[12]) ? mysqli_real_escape_string($con, $Row[12]) : "";
+                $RecetaMedica = isset($Row[13]) ? mysqli_real_escape_string($con, $Row[13]) : "";
+                $Licencia = isset($Row[14]) ? mysqli_real_escape_string($con, $Row[14]) : "";
+                $Ivaal16 = isset($Row[15]) ? mysqli_real_escape_string($con, $Row[15]) : "";
                 
                 if (!empty($Cod_Barra) || !empty($Nombre_Prod) || !empty($Precio_Venta)) {
-                    $query = "INSERT INTO ActualizacionesMasivasProductosPOS (Cod_Barra, Clave_adicional, Nombre_Prod, Precio_Venta, Precio_C, Componente_Activo, Tipo, FkCategoria, FkMarca, FkPresentacion, Proveedor1, Proveedor2, RecetaMedica, Licencia, Ivaal16, ActualizadoPor) VALUES ('$Cod_Barra', '$Clave_adicional', '$Nombre_Prod', '$Precio_Venta', '$Precio_C', '$Componente_Activo', '$Tipo', '$FkCategoria', '$FkMarca', '$FkPresentacion', '$Proveedor1', '$Proveedor2', '$RecetaMedica', '$Licencia', '$Ivaal16', '$ActualizadoPor')";
+                    $query = "INSERT INTO ActualizacionesMasivasProductosPOS (Id_Actualizado,Cod_Barra, Clave_adicional, Nombre_Prod, Precio_Venta, Precio_C, Componente_Activo, Tipo, FkCategoria, FkMarca, FkPresentacion, Proveedor1, Proveedor2, RecetaMedica, Licencia, Ivaal16, ActualizadoPor) VALUES ('$Id_Actualizado','$Cod_Barra', '$Clave_adicional', '$Nombre_Prod', '$Precio_Venta', '$Precio_C', '$Componente_Activo', '$Tipo', '$FkCategoria', '$FkMarca', '$FkPresentacion', '$Proveedor1', '$Proveedor2', '$RecetaMedica', '$Licencia', '$Ivaal16', '$ActualizadoPor')";
                     $resultados = mysqli_query($con, $query);
                 
                     if ($resultados) {
