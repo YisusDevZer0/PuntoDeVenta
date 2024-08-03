af24f66c9080671261ff8f919cc5775ebef4a8d9
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-02 21:57:40 -0600

    Update ControlCedis.php

--- PuntoDeVenta/ControlYAdministracion/ControlCedis.php
+++ PuntoDeVenta/ControlYAdministracion/ControlCedis.php
@@ -196,7 +196,7 @@ include_once "Controladores/ControladorUsuario.php";
   
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+    </div><!-- /.modal --> </div><!-- /.modal --> </div><!-- /.modal -->
             <!-- Footer Start -->
             <?php 
             include "Modales/NuevoFondoDeCaja.php";
