262dcfdb0be92f768370fb3c25b49276b7df67c0
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-06 21:40:29 -0600

    Update MarcaMensajeComoLeido.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Modales/MarcaMensajeComoLeido.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Modales/MarcaMensajeComoLeido.php
@@ -21,8 +21,7 @@ if ($id) {
 
 <?php if (isset($especialistas) && $especialistas) : ?>
     <form action="javascript:void(0)" method="post" id="MarcarLeidoForm">
-        <i id="lockIcon" class="fas fa-unlock fa-5x text-success"></i>
-        
+       
         <p>¿Estás seguro que deseas marcar como leído el siguiente mensaje?</p>
         <p><strong><?php echo $especialistas->Mensaje_Recordatorio; ?></strong></p>
         <input type="hidden" name="ID_Notificacion" id="ID_Notificacion" value="<?php echo $especialistas->ID_Notificacion; ?>">
@@ -32,15 +31,7 @@ if ($id) {
         </button>
     </form>
     <script src="js/DesactivaLaCaja.js"></script>
-    <script>
-        document.getElementById("submit").addEventListener("click", function() {
-            var lockIcon = document.getElementById("lockIcon");
-            lockIcon.classList.remove("fa-unlock");
-            lockIcon.classList.add("fa-lock");
-            lockIcon.classList.remove("text-success");
-            lockIcon.classList.add("text-danger");
-        });
-    </script>
+    
 <?php else : ?>
     <p class="alert alert-danger">404 No se encuentra el mensaje</p>
 <?php endif; ?>
