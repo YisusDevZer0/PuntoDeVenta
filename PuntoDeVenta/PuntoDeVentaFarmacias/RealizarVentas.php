9b52612db7f550f91090abcbe43d1ace39e08047
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-27 20:57:28 -0600

    Ajustes

--- PuntoDeVenta/PuntoDeVentaFarmacias/RealizarVentas.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/RealizarVentas.php
@@ -1485,7 +1485,7 @@ include("footer.php") ?>
 
 
 
-  <script src="js/FinalizaLasVentasSucursales.js"></script>
+  <script src="js/FinalizaLasVentasSucursaless.js"></script>
   
   <script src="js/BuscaDataPacientes.js"></script>
   <script src="js/BusquedaProductos.js"></script>
