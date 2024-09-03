0253933b5e7dc0b60c04ca0bb311d221cd05f8fe
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-02 20:26:41 -0600

    reimpresiones

--- PuntoDeVenta/PuntoDeVentaFarmacias/Tickets.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Tickets.php
@@ -43,7 +43,71 @@ include_once "Controladores/ControladorUsuario.php";
             
           
 <script src="js/DesgloseTicketss.js"></script>
+<script>
+    $(".btn-desglose").click(function(){
+    id = $(this).data("id");
+    $.post("https://saludapos.com/POS2/Modales/DesgloseTicket.php","id="+id,function(data){
+        $("#FormCancelacion").html(data);
+        $("#TituloCancelacion").html("Desglose del ticket");
+        $("#Di3").removeClass("modal-dialog modal-lg modal-notify modal-info");
+        $("#Di3").removeClass("modal-dialog modal-xl modal-notify modal-success");
+        $("#Di3").addClass("modal-dialog modal-xl modal-notify modal-primary");
+        var modal_lv = 0;
+          $('.modal').on('shown.bs.modal', function (e) {
+            $('.modal-backdrop:last').css('zIndex', 1051 + modal_lv);
+            $(e.currentTarget).css('zIndex', 1052 + modal_lv);
+            modal_lv++
+          });
 
+          $('.modal').on('hidden.bs.modal', function (e) {
+            modal_lv--
+          });
+    });
+    $('#Cancelacionmodal').modal('show');
+});
+
+$(".btn-Reimpresion").click(function(){
+    id = $(this).data("id");
+    $.post("https://doctorpez.mx/PuntoDeVenta/PuntoDeVentaFarmacias/Modales/ReimprimeTicketsVenta.php","id="+id,function(data){
+        $("#FormCancelacion").html(data);
+        $("#TituloCancelacion").html("Editar datos de categoría");
+        $("#Di3").removeClass("modal-dialog modal-lg modal-notify modal-info");
+        $("#Di3").removeClass("modal-dialog modal-xl modal-notify modal-primary");
+        $("#Di3").addClass("modal-dialog modal-xl modal-notify modal-success");
+        var modal_lv = 0;
+          $('.modal').on('shown.bs.modal', function (e) {
+            $('.modal-backdrop:last').css('zIndex', 1051 + modal_lv);
+            $(e.currentTarget).css('zIndex', 1052 + modal_lv);
+            modal_lv++
+          });
+
+          $('.modal').on('hidden.bs.modal', function (e) {
+            modal_lv--
+          });
+    });
+    $('#Cancelacionmodal').modal('show');
+});
+</script>
+
+ <div class="modal fade" id="Cancelacionmodal" tabindex="-2" role="dialog" style="overflow-y: scroll;" aria-labelledby="CancelacionmodalLabel" aria-hidden="true">
+  <div id="Di3" class="modal-dialog modal-lg modal-notify modal-info">
+      <div class="modal-content">
+      <div class="modal-header">
+         <p class="heading lead" id="TituloCancelacion">Confirmacion de ticket</p>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true" class="white-text">&times;</span>
+         </button>
+       </div>
+       
+	        <div class="modal-body">
+          <div class="text-center">
+        <div id="FormCancelacion"></div>
+        
+        </div>
+
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
             <!-- Footer Start -->
             <?php 
             include "Modales/NuevoFondoDeCaja.php";
