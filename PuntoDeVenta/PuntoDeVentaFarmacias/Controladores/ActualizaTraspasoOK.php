c9054dae7173badcaa3b1ba1faa9b61e940294aa
SaludaDev <saludasistemas@gmail.com>
2024-09-06 12:28:04 -0600

    ajuxts

---
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ActualizaTraspasoOK.php
@@ -0,0 +1,22 @@
+
+<?php
+include "db_connect.php";
+                $InfoState="Entregado";
+        $ID_Traspaso_Generado=$conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['IDTraspasoActualiza']))));
+       $TraspasoRecibidoPor= $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['NombreRecibio']))));
+        $Estatus= $conn -> real_escape_string(htmlentities(strip_tags(Trim($InfoState))));
+        $Fecha_recepcion= $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['FechaRecepcion']))));
+        $sql = "UPDATE `Traspasos_generados` 
+        SET `Estatus`='$Estatus',
+        `TraspasoRecibidoPor`='$TraspasoRecibidoPor',
+        `Fecha_recepcion`='$Fecha_recepcion'
+        WHERE ID_Traspaso_Generado=$ID_Traspaso_Generado";
+       if (mysqli_query($conn, $sql)) {
+		echo json_encode(array("statusCode"=>200));
+	} 
+	else {
+		echo json_encode(array("statusCode"=>201));
+	}
+	mysqli_close($conn);
+
+?>
---
+++ PuntoDeVenta/PuntoDeVentaFarmacias/js/ActualizaDataDeTraspasos.js
@@ -0,0 +1,128 @@
+$('document').ready(function ($) {
+    $.validator.addMethod("Sololetras", function (value, element) {
+      return this.optional(element) || /[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]*$/.test(value);
+    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+    $.validator.addMethod("Telefonico", function (value, element) {
+      return this.optional(element) || /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/.test(value);
+    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar numeros!");
+    $.validator.addMethod("Correos", function (value, element) {
+      return this.optional(element) || /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(value);
+    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Ingresa un correo valido!");
+    $.validator.addMethod("NEmpresa", function (value, element) {
+      return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
+    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+    $.validator.addMethod("Problema", function (value, element) {
+      return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
+    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+  
+  
+    $("#ActualizaEstadoTraspaso").validate({
+      rules: {
+  
+        CodBarra:{
+          required:true,
+        },
+        Clav: {
+          maxlength: 15,
+        },
+       
+      },
+      messages: {
+  
+        
+        NombreProd: {
+          required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
+          maxlength: "No puedes ingresar mas de 5 caracteres",
+          minlength: "Debes ingresar minimo 3 caracteres"
+        },
+        Sucursal: {
+          required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
+        },
+       
+      },
+      submitHandler: submitForm
+    });
+    // hide messages 
+  
+  
+    function submitForm() {
+  
+  
+  
+      $.ajax({
+        type: 'POST',
+        url: "Controladores/ActualizaTraspasoOK.php",
+        data: $('#ActualizaEstadoTraspaso').serialize(),
+        cache: false,
+        beforeSend: function () {
+  
+          $("#submit_registro").html("Verificando datos... <span class='fa fa-refresh fa-spin' role='status' aria-hidden='true'></span>");
+  
+  
+        },
+        success: function (dataResult) {
+          var dataResult = JSON.parse(dataResult);
+  
+          if (dataResult.statusCode == 250) {
+            var modal_lv = 0;
+            $('.modal').on('shown.bs.modal', function (e) {
+              $('.modal-backdrop:last').css('zIndex', 1051 + modal_lv);
+              $(e.currentTarget).css('zIndex', 1052 + modal_lv);
+              modal_lv++
+            });
+  
+            $('.modal').on('hidden.bs.modal', function (e) {
+              modal_lv--
+            });
+            $("#submit_registro").html("Algo no salio bien.. <i class='fas fa-exclamation-triangle'></i>");
+            $('#ErrorDupli').modal('toggle');
+            setTimeout(function () {
+            }, 2000); // abrir
+            setTimeout(function () {
+              $("#submit_registro").html("Guardar <i class='fas fa-save'></i>");
+            }, 3000); // abrir
+  
+  
+          }
+  
+          else if (dataResult.statusCode == 200) {
+  
+            $("#submit_registro").html("Enviado <i class='fas fa-check'></i>")
+  
+            $("#ActualizaEstadoTraspaso")[0].reset();
+            $("#AltaProductos").removeClass("in");
+            $(".modal-backdrop").remove();
+            $("#AltaProductos").hide();
+          
+  
+            $('#TraspasoCompleto').modal('toggle');
+            setTimeout(function () {
+              $('#TraspasoCompleto').modal('hide')
+            }, 3000); // abrir
+            setTimeout(' window.location.href = "https://doctorpez.mx/PuntoDeVenta/PuntoDeVentaFarmacias/ListadoDeTraspasos"; ',3000);
+  
+  
+            //  Solucionar muestra de modal de TraspasoCompleto
+  
+  
+          }
+          else if (dataResult.statusCode == 201) {
+            $("#submit_Age").html("Algo no salio bien.. <i class='fas fa-exclamation-triangle'></i>");
+            $('#ErrorData').modal('toggle');
+  
+            setTimeout(function () {
+              $("#submit_Age").html("Guardar <i class='fas fa-save'></i>");
+            }, 3000); // abrir
+  
+  
+          }
+  
+  
+  
+  
+  
+        }
+      });
+      return false;
+    }
+  });
