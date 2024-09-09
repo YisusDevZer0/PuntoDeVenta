266d54088e647769c43c3a57693e99a9265969ac
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-08 20:58:00 -0600

    Update GenerarNuevoTraspasoSucursales.php

--- PuntoDeVenta/ControlYAdministracion/Modales/GenerarNuevoTraspasoSucursales.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/GenerarNuevoTraspasoSucursales.php
@@ -18,7 +18,7 @@
    
   
   ?>
-      <div class="modal fade bd-example-modal-xl" id="GeneradorDeOrdenesDeTraspaso" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+      <div class="modal fade bd-example-modal-xl" id="GeneradorDeOrdenesDeTraspasoSucursales" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
       <div class="modal-dialog  modal-notify modal-success">
     <div class="modal-content">
       <div class="modal-header" style=" background-color: #ef7980 !important;">
