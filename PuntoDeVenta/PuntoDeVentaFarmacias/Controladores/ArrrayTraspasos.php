595689f85bcdeed92e757b18b274f491fbe7dc74
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-17 23:27:39 -0600

    Update ArrrayTraspasos.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrrayTraspasos.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrrayTraspasos.php
@@ -3,34 +3,39 @@ header('Content-Type: application/json');
 include("db_connect.php");
 include_once "ControladorUsuario.php";
 
+// Escapar la variable para evitar inyección SQL
+$fk_sucursal = mysqli_real_escape_string($conn, $row['Fk_Sucursal']);
+
+// Modificar la consulta para usar la variable
 $sql = "SELECT 
-Traspasos_generados.ID_Traspaso_Generado,
-Traspasos_generados.TraspasoRecibidoPor,
-Traspasos_generados.TraspasoGeneradoPor,
-Traspasos_generados.Num_Orden,
-Traspasos_generados.Num_Factura,
-Traspasos_generados.TotaldePiezas,
-Traspasos_generados.Cod_Barra,
-Traspasos_generados.Nombre_Prod,
-Traspasos_generados.Fk_SucDestino,
-Traspasos_generados.Precio_Venta,
-Traspasos_generados.Precio_Compra,
-Traspasos_generados.Cantidad_Enviada,
-Traspasos_generados.FechaEntrega,
-Traspasos_generados.Estatus,
-Traspasos_generados.ID_H_O_D,
-Sucursales.ID_Sucursal,
-Sucursales.Nombre_Sucursal 
+    Traspasos_generados.ID_Traspaso_Generado,
+    Traspasos_generados.TraspasoRecibidoPor,
+    Traspasos_generados.TraspasoGeneradoPor,
+    Traspasos_generados.Num_Orden,
+    Traspasos_generados.Num_Factura,
+    Traspasos_generados.TotaldePiezas,
+    Traspasos_generados.Cod_Barra,
+    Traspasos_generados.Nombre_Prod,
+    Traspasos_generados.Fk_SucDestino,
+    Traspasos_generados.Precio_Venta,
+    Traspasos_generados.Precio_Compra,
+    Traspasos_generados.Cantidad_Enviada,
+    Traspasos_generados.FechaEntrega,
+    Traspasos_generados.Estatus,
+    Traspasos_generados.ID_H_O_D,
+    Sucursales.ID_Sucursal,
+    Sucursales.Nombre_Sucursal
 FROM 
-Traspasos_generados,
-Sucursales
+    Traspasos_generados,
+    Sucursales
 WHERE 
-Traspasos_generados.Fk_SucDestino = Sucursales.ID_Sucursal 
-AND (
-    (MONTH(Traspasos_generados.FechaEntrega) = MONTH(CURDATE()) AND YEAR(Traspasos_generados.FechaEntrega) = YEAR(CURDATE()))
-    OR
-    (MONTH(Traspasos_generados.FechaEntrega) = MONTH(CURDATE() - INTERVAL 1 MONTH) AND YEAR(Traspasos_generados.FechaEntrega) = YEAR(CURDATE() - INTERVAL 1 MONTH))
-)"; 
+    Traspasos_generados.Fk_SucDestino = Sucursales.ID_Sucursal
+    AND Traspasos_generados.Fk_SucDestino = '$fk_sucursal' -- Usar la variable en la consulta
+    AND (
+        (MONTH(Traspasos_generados.FechaEntrega) = MONTH(CURDATE()) AND YEAR(Traspasos_generados.FechaEntrega) = YEAR(CURDATE()))
+        OR
+        (MONTH(Traspasos_generados.FechaEntrega) = MONTH(CURDATE() - INTERVAL 1 MONTH) AND YEAR(Traspasos_generados.FechaEntrega) = YEAR(CURDATE() - INTERVAL 1 MONTH))
+    )"; 
 
 $result = mysqli_query($conn, $sql);
 
@@ -44,12 +49,10 @@ if ($result && mysqli_num_rows($result) > 0) {
         $data[$c]["Cod_Barra"] = $fila["Cod_Barra"];
         $data[$c]["NumOrden"] = $fila["Num_Orden"];
         $data[$c]["Nombre_Prod"] = $fila["Nombre_Prod"];
-      
         $data[$c]["Cantidad"] = $fila["Cantidad_Enviada"];
         $data[$c]["FechaEntrega"] = fechaCastellano($fila["FechaEntrega"]);
         $data[$c]["Estatus"] = fechaCastellano($fila["Estatus"]);
         $data[$c]["Traspasocorrecto"] = ["<a data-id='$fila[ID_Traspaso_Generado]' class='btn btn-success btn-sm btn-AceptarTraspaso'><i class='fa-solid fa-circle-check'></i></a>"];
-
         $c++;
     }
 }
@@ -62,21 +65,4 @@ $results = [
 ];
 
 echo json_encode($results);
-?>
-<?php
-
-function fechaCastellano ($fecha) {
-  $fecha = substr($fecha, 0, 10);
-  $numeroDia = date('d', strtotime($fecha));
-  $dia = date('l', strtotime($fecha));
-  $mes = date('F', strtotime($fecha));
-  $anio = date('Y', strtotime($fecha));
-  $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-  $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-  $nombredia = str_replace($dias_EN, $dias_ES, $dia);
-$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-  $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-  $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
-  return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
-}
 ?>
