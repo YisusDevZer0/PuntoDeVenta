d26e57c60737e2d17d14f134a3110e91c7115c88
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-17 23:22:27 -0600

    Update GeneradorTraspasosSucursales.php

--- PuntoDeVenta/ControlYAdministracion/GeneradorTraspasosSucursales.php
+++ PuntoDeVenta/ControlYAdministracion/GeneradorTraspasosSucursales.php
@@ -899,10 +899,10 @@ function calcularDiferencia(fila) {
         <td style="display:none;" class="ResponsableInventario"><input hidden id="VendedorFarma" type="text" class="form-control" name="AgregoElVendedor[]" readonly value="<?php echo $row['Nombre_Apellidos']; ?>" /></td>
         <td style="display:none;" class="Sucursal"><input hidden type="text" class="form-control" name="Fk_sucursal[]" readonly value="<?php echo $row['Fk_Sucursal']; ?>" /></td>
         <td style="display:none;" class="Empresa"><input hidden type="text" class="form-control" name="Sistema[]" readonly value="POS" /></td>
-        <td style="display:none;" class="Empresa"><input hidden type="text" class="form-control" name="ID_H_O_D[]" readonly value="Saluda" /></td>
+        <td style="display:none;" class="Empresa"><input hidden type="text" class="form-control" name="ID_H_O_D[]" readonly value="Doctor Pez" /></td>
         <td style="display:none;" class="Fecha"><input hidden type="text" class="form-control" name="FechaAprox[]" readonly value="<?php echo $fechaActual; ?>" /></td>
         <td><div class="btn-container"><button type="button" class="btn btn-danger btn-sm" onclick="eliminarFila(this);"><i class="fas fa-minus-circle fa-xs"></i></button></div></td>
-       <td style="display:none;"> <input type="text" name="SucursalTraspasa[]" hidden value="21" class="form-control" ></td>
+       <td style="display:none;"> <input type="text" name="SucursalTraspasa[]" hidden value="<?php echo $row['SucursalOrigen']?>" class="form-control" ></td>
      <td style="display:none;"><input type="text" class="form-control "  hidden name="GeneradoPor[]" value="<?php echo $row['Nombre_Apellidos']?>"readonly  ></td>
        <td style="display:none;"> <input type="text" class="form-control " hidden  name="Empresa[]" value="<?php echo $row['Licencia']?>"readonly  ></td>
 
