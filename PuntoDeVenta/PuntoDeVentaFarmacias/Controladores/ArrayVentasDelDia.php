d451533d5be46fd0b67f5288fbc89b1969a289a5
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-22 21:22:39 -0600

    Update ArrayVentasDelDia.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayVentasDelDia.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayVentasDelDia.php
@@ -1,75 +1,68 @@
-
 <?php
 header('Content-Type: application/json');
 include("db_connect.php");
 include "ControladorUsuario.php";
 
+// Accede directamente a la variable Fk_Sucursal
+$sucursal = $row['Fk_Sucursal']; // Asegúrate de que $row esté disponible y tenga el valor correcto
 
 $sql = "SELECT DISTINCT
-Ventas_POS.Venta_POS_ID,
-Ventas_POS.Folio_Ticket,
-Ventas_POS.FolioSucursal,
-Ventas_POS.Fk_Caja,
-Ventas_POS.Identificador_tipo,
-Ventas_POS.Fecha_venta, 
-Ventas_POS.Total_Venta,
-Ventas_POS.Importe,
-Ventas_POS.Total_VentaG,
-Ventas_POS.FormaDePago,
-Ventas_POS.Turno,
-Ventas_POS.FolioSignoVital,
-Ventas_POS.Cliente,
-Cajas.ID_Caja,
-Cajas.Sucursal,
-Cajas.MedicoEnturno,
-Cajas.EnfermeroEnturno,
-Ventas_POS.Cod_Barra,
-Ventas_POS.Clave_adicional,
-Ventas_POS.Nombre_Prod,
-Ventas_POS.Cantidad_Venta,
-Ventas_POS.Fk_sucursal,
-Ventas_POS.AgregadoPor,
-Ventas_POS.AgregadoEl,
-Ventas_POS.Lote,
-Ventas_POS.ID_H_O_D,
-Sucursales.ID_Sucursal, 
-Sucursales.Nombre_Sucursal,
-Servicios_POS.Servicio_ID,
-Servicios_POS.Nom_Serv,
-Ventas_POS.DescuentoAplicado,
-Stock_POS.ID_Prod_POS,
-Stock_POS.Precio_Venta,
-Stock_POS.Precio_C
+    Ventas_POS.Venta_POS_ID,
+    Ventas_POS.Folio_Ticket,
+    Ventas_POS.FolioSucursal,
+    Ventas_POS.Fk_Caja,
+    Ventas_POS.Identificador_tipo,
+    Ventas_POS.Fecha_venta, 
+    Ventas_POS.Total_Venta,
+    Ventas_POS.Importe,
+    Ventas_POS.Total_VentaG,
+    Ventas_POS.FormaDePago,
+    Ventas_POS.Turno,
+    Ventas_POS.FolioSignoVital,
+    Ventas_POS.Cliente,
+    Cajas.ID_Caja,
+    Cajas.Sucursal,
+    Cajas.MedicoEnturno,
+    Cajas.EnfermeroEnturno,
+    Ventas_POS.Cod_Barra,
+    Ventas_POS.Clave_adicional,
+    Ventas_POS.Nombre_Prod,
+    Ventas_POS.Cantidad_Venta,
+    Ventas_POS.Fk_sucursal,
+    Ventas_POS.AgregadoPor,
+    Ventas_POS.AgregadoEl,
+    Ventas_POS.Lote,
+    Ventas_POS.ID_H_O_D,
+    Sucursales.ID_Sucursal, 
+    Sucursales.Nombre_Sucursal,
+    Servicios_POS.Servicio_ID,
+    Servicios_POS.Nom_Serv,
+    Ventas_POS.DescuentoAplicado,
+    Stock_POS.ID_Prod_POS,
+    Stock_POS.Precio_Venta,
+    Stock_POS.Precio_C
 FROM 
-Ventas_POS
+    Ventas_POS
 LEFT JOIN 
-Sucursales ON Ventas_POS.Fk_sucursal = Sucursales.ID_Sucursal 
+    Sucursales ON Ventas_POS.Fk_sucursal = Sucursales.ID_Sucursal 
 LEFT JOIN 
-Servicios_POS ON Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID 
+    Servicios_POS ON Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID 
 LEFT JOIN 
-Cajas ON Cajas.ID_Caja = Ventas_POS.Fk_Caja
+    Cajas ON Cajas.ID_Caja = Ventas_POS.Fk_Caja
 LEFT JOIN 
-Stock_POS ON Stock_POS.ID_Prod_POS = Ventas_POS.ID_Prod_POS
+    Stock_POS ON Stock_POS.ID_Prod_POS = Ventas_POS.ID_Prod_POS
 WHERE 
-YEAR(Ventas_POS.Fecha_venta) = YEAR(CURDATE()) 
-AND MONTH(Ventas_POS.Fecha_venta) = MONTH(CURDATE());
-
-";
-
-
-
+    YEAR(Ventas_POS.Fecha_venta) = YEAR(CURDATE()) 
+    AND MONTH(Ventas_POS.Fecha_venta) = MONTH(CURDATE())
+    AND Ventas_POS.Fk_sucursal = '$sucursal';"; // Filtro por la sucursal
 
 $result = mysqli_query($conn, $sql);
 
- 
-$c=0;
- 
-while($fila=$result->fetch_assoc()){
- 
+$c = 0;
+
+while ($fila = $result->fetch_assoc()) {
     $data[$c]["Cod_Barra"] = $fila["Cod_Barra"];
     $data[$c]["Nombre_Prod"] = $fila["Nombre_Prod"];
-  
-
     $data[$c]["FolioTicket"] = $fila["FolioSucursal"] . '' . $fila["Folio_Ticket"];
     $data[$c]["Sucursal"] = $fila["Nombre_Sucursal"];
     $data[$c]["Turno"] = $fila["Turno"];
@@ -84,15 +77,16 @@ while($fila=$result->fetch_assoc()){
     $data[$c]["AgregadoEl"] = date("d/m/Y", strtotime($fila["Fecha_venta"]));
     $data[$c]["AgregadoEnMomento"] = $fila["AgregadoEl"];
     $data[$c]["AgregadoPor"] = $fila["AgregadoPor"];
-  
-    $c++; 
- 
+
+    $c++;
 }
- 
-$results = ["sEcho" => 1,
-            "iTotalRecords" => count($data),
-            "iTotalDisplayRecords" => count($data),
-            "aaData" => $data ];
- 
+
+$results = [
+    "sEcho" => 1,
+    "iTotalRecords" => count($data),
+    "iTotalDisplayRecords" => count($data),
+    "aaData" => $data
+];
+
 echo json_encode($results);
 ?>
