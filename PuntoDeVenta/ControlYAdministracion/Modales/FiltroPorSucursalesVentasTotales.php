f4340db2d918b620c5f5bf52313c8e85a0b9dfc6
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-09-25 23:10:43 -0600

    Update FiltroPorSucursalesVentasTotales.php

--- PuntoDeVenta/ControlYAdministracion/Modales/FiltroPorSucursalesVentasTotales.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/FiltroPorSucursalesVentasTotales.php
@@ -1,5 +1,6 @@
 <div class="modal fade bd-example-modal-xl" id="FiltroPorSucursalesVentasEnTotal" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-notify modal-success">
+
+<div class="modal-dialog modal-notify modal-success">
     <div class="modal-content">
     
     <div class="text-center">
@@ -13,6 +14,7 @@
       <div class="modal-body">
         <form action="javascript:void(0)" method="post" id="CambiaDeSucursal">
           <div class="row">
+            
             <!-- Mes dinámico -->
             <div class="col">
               <label for="mesSelect">Mes actual</label>
@@ -46,6 +48,27 @@
                   <span class="input-group-text" id="anioIcon"><i class="far fa-calendar"></i></span>
                 </div>
                 <select id="anioSelect" class="form-control" name="Anio" required></select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <!-- Sucursal a elegir -->
+            <div class="col">
+              <label for="sucursal">Sucursal a elegir</label>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="sucursalIcon"><i class="far fa-hospital"></i></span>
+                </div>
+                <select id="sucursal" class="form-control" name="Sucursal" required>
+                  <option value="">Seleccione una Sucursal:</option>
+                  <?php 
+                    $query = $conn -> query ("SELECT ID_Sucursal,Nombre_Sucursal,Licencia FROM Sucursales WHERE Licencia='".$row['Licencia']."'");
+                    while ($valores = mysqli_fetch_array($query)) {
+                      echo '<option value="'.$valores["ID_Sucursal"].'">'.$valores["Nombre_Sucursal"].'</option>';
+                    }
+                  ?>
+                </select>
               </div>
             </div>
           </div>
