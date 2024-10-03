59e54afc7a3c2674168ba00a49ca9927794c5d69
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-10-03 08:16:39 -0600

    Ajustes

--- PuntoDeVenta/ControlYAdministracion/ReporteVentasTotalesPorFechas.php
+++ PuntoDeVenta/ControlYAdministracion/ReporteVentasTotalesPorFechas.php
@@ -56,8 +56,8 @@ include_once "Controladores/ControladorUsuario.php";
 
               // Realizar las operaciones que necesites con estas variables
               // Por ejemplo, imprimir su valor
-              echo "Mes seleccionado: $mes<br>";
-              echo "Año seleccionado: $anual<br>";
+              echo "Fecha inicial: $mes<br>";
+              echo "Fecha final: $anual<br>";
           } else {
               // Si alguna de las variables no está seteada o es nula, mostrar un mensaje de error
               echo "Error: No se recibieron todas las variables necesarias.";
@@ -280,88 +280,121 @@ include_once "Controladores/ControladorUsuario.php";
   }
 
 
-tabla = $('#Clientes').DataTable({
+  var tabla;
+        $(document).ready(function() {
+          tabla = $('#Productos').DataTable({
+            "processing": true,
+            "ordering": true,
+            "stateSave": true,
+            "autoWidth": true,
+            "order": [[ 0, "desc" ]],
+            "ajax": {
+              "type": "POST", // Especifica el método de envío de la solicitud AJAX
+              "url": "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/ArrayVentasPorTotalesPorFechas.php",
 
- "bProcessing": true,
- "ordering": true,
- "stateSave":true,
- "bAutoWidth": false,
- "order": [[ 0, "desc" ]],
- "sAjaxSource": "https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/Controladores/ArrayVentasPorTotales.php",
- "aoColumns": [
+              "data": function (d) {
+        // Aquí puedes definir el código PHP directamente
+        var mes = '<?php echo $mes; ?>'; // Obtén el valor de mes desde PHP
+        var anual = '<?php echo $anual; ?>'; // Obtén el valor de anual desde PHP
 
-  { mData: 'Cod_Barra' },
-  { mData: 'Nombre_Prod' },
-  { mData: 'Nombre_Sucursal' },
-  { mData: 'Folio_Ticket' },
-  { mData: 'Turno' },
-  
+        // Construye el objeto de datos para enviar al servidor
+        var dataToSend = {
+            "Mes": mes,
+            "anual": anual
+        };
 
-           
-            { mData: 'Cantidad_Venta' },
-            
-            { mData: 'Total_Venta' },
-            { mData: 'Importe' },
-            { mData: 'Importetarjeta' },
-            { mData: 'Importecredito' },
-            { mData: 'Total_VentaG' },
-            { mData: 'DescuentoAplicado' },
-            { mData: 'FormaDePago' },
-           
-            { mData: 'Cambio' },
-            { mData: 'Cliente' },
-            { mData: 'Fecha_venta' },
-           
-            { mData: 'AgregadoPor' },
-            { mData: 'AgregadoEl' },
-         
-           
-           
-      ],
-     
-      "lengthMenu": [[20,150,250,500, -1], [20,50,250,500, "Todos"]],  
-  
-  "language": {
-  "lengthMenu": "Mostrar _MENU_ registros",
-  "sPaginationType": "extStyle",
-  "zeroRecords": "No se encontraron resultados",
-  "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-  "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-  "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-  "sSearch": "Buscar:",
-  "paginate": {
-    "first": '<i class="fas fa-angle-double-left"></i>',
-    "last": '<i class="fas fa-angle-double-right"></i>',
-    "next": '<i class="fas fa-angle-right"></i>',
-    "previous": '<i class="fas fa-angle-left"></i>'
-  },
-  "processing": function () {
-    mostrarCargando();
-  }
-},
-"initComplete": function() {
-  // Al completar la inicialización de la tabla, ocultar el mensaje de carga
-  ocultarCargando();
-},
-// Para personalizar el estilo del botón de Excel
-"buttons": [
-  {
-    extend: 'excelHtml5',
-    text: 'Exportar a Excel  <i class="fas fa-file-excel"></i> ',
-    titleAttr: 'Exportar a Excel',
-    title: 'Ventas totales del mes en curso',
-    className: 'btn btn-excel',  // Añadir una clase personalizada
-    exportOptions: {
-      columns: ':visible' // Exportar solo las columnas visibles
-    }
-  }
-],
+        return dataToSend;
+    },
+              "error": function(xhr, error, thrown) {
+            console.log("Error en la solicitud AJAX:", error);
+        }
+    },
+            "columns": [
+                { "data": "Cod_Barra" },
+{ "data": "Nombre_Prod" },
+{ "data": "Nombre_Sucursal" },
+{ "data": "Folio_Ticket" },
+{ "data": "Turno" },
 
-// Personalizar la posición de los elementos del encabezado
-"dom": '<"d-flex justify-content-between"lBf>rtip', // Modificar la disposición aquí
-"responsive": true
-});
-</script>
+{ "data": "Cantidad_Venta" },
+
+{ "data": "Total_Venta" },
+{ "data": "Importe" },
+{ "data": "Importetarjeta" },
+{ "data": "Importecredito" },
+{ "data": "Total_VentaG" },
+{ "data": "DescuentoAplicado" },
+{ "data": "FormaDePago" },
+
+{ "data": "Cambio" },
+{ "data": "Cliente" },
+{ "data": "Fecha_venta" },
+
+{ "data": "AgregadoPor" },
+{ "data": "AgregadoEl" }
+
+            ],
+            "lengthMenu": [[10,20,150,250,500, -1], [10,20,50,250,500, "Todos"]],
+            "language": {
+              "lengthMenu": "Mostrar _MENU_ registros",
+              "sPaginationType": "extStyle",
+              "zeroRecords": "No se encontraron resultados",
+              "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+              "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+              "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+              "sSearch": "Buscar:",
+              "paginate": {
+                "first": '<i class="fas fa-angle-double-left"></i>',
+                "last": '<i class="fas fa-angle-double-right"></i>',
+                "next": '<i class="fas fa-angle-right"></i>',
+                "previous": '<i class="fas fa-angle-left"></i>'
+              },
+              "processing": function () {
+                mostrarCargando();
+              }
+            },
+            "initComplete": function() {
+              // Al completar la inicialización de la tabla, ocultar el mensaje de carga
+              ocultarCargando();
+            },
+            "buttons": [
+      {
+        text: 'Exportar a Excel <i class="fas fa-file-excel"></i>',
+titleAttr: 'Exportar a Excel',
+className: 'btn btn-success',
+action: function(e, dt, button, config) {
+    // Mostrar una alerta con SweetAlert2
+    Swal.fire({
+        title: 'Advertencia',
+        html: 'Estimado usuario, te comentamos que el archivo se descarga en formato .csv. Te recomendamos que, una vez completada la descarga, lo conviertas al formato necesario que requieras.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Exportar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Crear y enviar el formulario si el usuario confirma
+            var form = $('<form method="POST" action="https://saludapos.com/AdminPOS/Consultas/ExportToExcel" target="_blank"></form>');
+            var mesInput = $('<input type="hidden" name="Mes" value="' + mes + '">');
+            var anualInput = $('<input type="hidden" name="anual" value="' + anual + '">');
+            form.append(mesInput);
+            form.append(anualInput);
+            $('body').append(form);
+            form.submit();
+        }
+    });
+}
+
+      }
+    ],
+            "dom": '<"d-flex justify-content-between"lBf>rtip', // Modificar la disposición aquí
+            "responsive": true
+          });
+        });
+      </script>
+
+
+
 <div class="text-center">
   <div class="table-responsive">
   <table  id="Clientes"  class="order-column">
