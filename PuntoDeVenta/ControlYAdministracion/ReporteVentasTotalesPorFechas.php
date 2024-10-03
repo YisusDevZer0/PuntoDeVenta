4cd31220f76abd406ebdb6fc3a6f8d4c692952a7
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-10-03 07:51:27 -0600

    Update ReporteVentasTotalesPorFechas.php

--- PuntoDeVenta/ControlYAdministracion/ReporteVentasTotalesPorFechas.php
+++ PuntoDeVenta/ControlYAdministracion/ReporteVentasTotalesPorFechas.php
@@ -394,13 +394,8 @@ action: function(e, dt, button, config) {
             include "Modales/Modales_Errores.php";
             include "Modales/Modales_Referencias.php";
             include "Footer.php";?>
-<script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>  
-    <script src="datatables/JSZip-2.5.0/jszip.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
-    <script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-t>
+
+
   
 </body>
 </html>
