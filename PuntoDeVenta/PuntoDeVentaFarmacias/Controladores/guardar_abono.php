0abb7f9f7eaaa7183875315b6d39a146b11db2c3
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-10-13 17:06:56 -0600

    Update guardar_abono.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/guardar_abono.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/guardar_abono.php
@@ -1,9 +1,32 @@
 <?php
-// Mostrar todos los datos que llegan en la petición POST
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+include "../Controladores/db_connect.php";
 
-// Esto detendrá la ejecución, asegurando que puedas ver los datos que están llegando.
-exit();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $FkCaja = $_POST['FkCaja'];
+    $Turno = $_POST['Turno'];
+    $SaldoPrevio = $_POST['AbonoPendiente'];
+    $Abono = $_POST['Abonado'];
+    $NuevoSaldo = $_POST['NuevoSaldo'];
+    $CobradoPor = $_POST['CobradoPor'];
+    $FormaPago = $_POST['FormaPago'];
+    $NumTicket = $_POST['TicketAnterior'];
+    $TicketNuevo = $_POST['TicketNuevo']; // Esto dependerá de tu lógica
+
+    $FechaHora = date("Y-m-d H:i:s");
+
+    $sql = "INSERT INTO AbonosCreditosVentas 
+        (FkCaja, Turno, SaldoPrevio, Abono, NuevoSaldo, CobradoPor, FormaPago, NumTicket, TicketNuevo, FechaHora) 
+        VALUES 
+        ('$FkCaja', '$Turno', '$SaldoPrevio', '$Abono', '$NuevoSaldo', '$CobradoPor', '$FormaPago', '$NumTicket', '$TicketNuevo', '$FechaHora')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Abono registrado con éxito";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+} else {
+    echo "Acceso denegado";
+}
 ?>
