420edd8a6162fd42da48abe8eb92cc0ad4aa223d
SaludaDev <jesusemutul@gmail.com>
2024-10-20 12:14:19 -0600

    Update index.php

--- PuntoDeVenta/ControlYAdministracion/index.php
+++ PuntoDeVenta/ControlYAdministracion/index.php
@@ -1,6 +1,10 @@
 <?php
 include_once "Controladores/ControladorUsuario.php";
 include_once "Controladores/ConsultaDashboard.php";
+include_once "Controladores/ConsultaCajasDasboard.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 ?>
