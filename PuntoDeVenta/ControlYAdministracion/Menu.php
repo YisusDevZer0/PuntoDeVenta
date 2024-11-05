bd798f83e0ffe11edd9bd964d479ab71f650e921
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-11-04 19:11:47 -0600

    Ajustes

--- PuntoDeVenta/ControlYAdministracion/Menu.php
+++ PuntoDeVenta/ControlYAdministracion/Menu.php
@@ -137,7 +137,7 @@
                         <div class="dropdown-menu bg-transparent border-0">
                             <a href="InventarioCedis" class="dropdown-item"><i class="fa-solid fa-warehouse"></i>Inventario de cedis</a>
                             <a href="InventarioSucursales" class="dropdown-item"><i class="fa-solid fa-boxes-stacked"></i>inventario de sucursales </a>
-                            <a href="AjusteInventarios" class="dropdown-item"><i class="fa-solid fa-plus-minus"></i>Ajuste de inventarios </a>
+                            <!-- <a href="AjusteInventarios" class="dropdown-item"><i class="fa-solid fa-plus-minus"></i>Ajuste de inventarios </a> -->
                             <a href="AjusteInsumos" class="dropdown-item"><i class="fa-solid fa-money-bills"></i>Insumos </a>
                             <a href="ReportesCedisInventario" class="dropdown-item"><i class="fa-solid fa-money-bills"></i>Reportes Cedis </a>
                             <a href="ReportesInventarios" class="dropdown-item"><i class="fa-solid fa-square-poll-vertical"></i>Reportes inventarios </a>
--- PuntoDeVenta/PuntoDeVentaFarmacias/CortesDeCaja.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/CortesDeCaja.php
@@ -35,7 +35,7 @@ include_once "Controladores/ControladorUsuario.php";
             <div class="container-fluid pt-4 px-4">
     <div class="col-12">
         <div class="bg-light rounded h-100 p-4">
-            <h6 class="mb-4">Fondos de caja de <?php echo $row['Licencia']?></h6>
+        <h6 class="mb-4" style="color:#0172b6;">Cortes de caja <?php echo $row['Licencia']?> Sucursal <?php echo $row['Nombre_Sucursal']?></h6>
            
             <div id="FCajas"></div>
             </div></div></div></div>
