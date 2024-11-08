78f405d5e0bf6b90850f287c05facc47f112fd4c
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-11-07 20:11:42 -0600

    Update ArrayStocks.php

--- PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayStocks.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Controladores/ArrayStocks.php
@@ -8,10 +8,10 @@ $licencia = isset($row['Licencia']) ? $row['Licencia'] : '';
 $Fk_Sucursal = isset($row['Fk_Sucursal']) ? $row['Fk_Sucursal'] : '';
 
 // Consulta segura utilizando una sentencia preparada
-$sql = "SELECT Stock_POS.Folio_Prod_Stock, Stock_POS.Clave_adicional, Stock_POS.ID_Prod_POS, Stock_POS.AgregadoEl, Stock_POS.Clave_adicional, Stock_POS.Clave_Levic,
-Stock_POS.Cod_Barra, Stock_POS.Nombre_Prod, Stock_POS.Tipo_Servicio, Stock_POS.Tipo, Stock_POS.Fk_sucursal,
-Stock_POS.Max_Existencia, Stock_POS.Min_Existencia, Stock_POS.Existencias_R, Stock_POS.Proveedor1,
-Stock_POS.Proveedor2, Stock_POS.Estatus, Stock_POS.ID_H_O_D, Sucursales.ID_Sucursal,
+$sql = "SELECT Stock_POS.Folio_Prod_Stock,Stock_POS.Clave_adicional,Stock_POS.ID_Prod_POS,Stock_POS.AgregadoEl,Stock_POS.Clave_adicional,Stock_POS.Clave_Levic,
+Stock_POS.Cod_Barra,Stock_POS.Nombre_Prod,Stock_POS.Tipo_Servicio,Stock_POS.Tipo,Stock_POS.Fk_sucursal,
+Stock_POS.Max_Existencia,Stock_POS.Min_Existencia, Stock_POS.Existencias_R,Stock_POS.Proveedor1,
+Stock_POS.Proveedor2,Stock_POS.Estatus,Stock_POS.ID_H_O_D, Sucursales.ID_Sucursal,
 Sucursales.Nombre_Sucursal, Servicios_POS.Servicio_ID, Servicios_POS.Nom_Serv, Productos_POS.ID_Prod_POS,
 Productos_POS.Precio_Venta, Productos_POS.Precio_C 
 FROM Stock_POS
