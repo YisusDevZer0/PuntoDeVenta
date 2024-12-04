b91a79c784352d03600661f45b31080a2da81353
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-12-03 18:46:36 -0600

    Update index.php

--- PuntoDeVenta/ControlYAdministracion/index.php
+++ PuntoDeVenta/ControlYAdministracion/index.php
@@ -67,7 +67,7 @@ echo "MX$ " . $formattedTotal; ?></h6>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-xl-3">
+                    <!-- <div class="col-sm-6 col-xl-3">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-pie fa-3x text-primary"></i>
                             <div class="ms-3">
@@ -75,7 +75,7 @@ echo "MX$ " . $formattedTotal; ?></h6>
                                 <h6 class="mb-0">$1234</h6>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <!-- Sale & Revenue End -->
