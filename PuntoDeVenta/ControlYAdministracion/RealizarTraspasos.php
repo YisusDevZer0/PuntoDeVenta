9fa119edbb2cc202e8e83dfc7b88fae423dfdb21
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2024-12-28 17:02:51 -0600

    Update RealizarTraspasos.php

--- PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
+++ PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
@@ -1416,24 +1416,6 @@ $('#abrirSweetAlertBtn').on('click', function() {
 
 
 
-<?php
-if ($ValorCaja["Estatus"] == 'Abierta') {
-} else {
-
-  echo '
-      <script>
-$(document).ready(function()
-{
-  // id de nuestro modal
-  $("#NoCaja").modal("show");
-  $("#codigoEscaneado").prop("disabled", true); // Desactivar el input con el ID "miInput"
-});
-</script>
-      ';
-}
-
-?>
-
 
 <!-- Control Sidebar -->
 
