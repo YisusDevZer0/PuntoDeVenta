ab85a34695161d456d651eb6b7c65bd6b34c75ab
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2024-12-28 18:07:12 -0600

    Update RealizarTraspasos.php

--- PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
+++ PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
@@ -254,7 +254,7 @@ $resultado_en_mayusculas = strtoupper($resultado_concatenado);
 
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
               <li class="nav-item">
-                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true" style="font-weight: 600;">Ventas</a>
+                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true" style="font-weight: 600;">Realizar Traspasos</a>
               </li>
               
               
