4f6e89c72fe8b02fcc44b6318d304b8d6db25883
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2025-01-05 12:07:38 -0600

    Update RealizarTraspasos.php

--- PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
+++ PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
@@ -933,7 +933,6 @@ $('#codigoEscaneado').autocomplete({
         tr += '<td style="visibility:collapse; display:none;" class="Empresa"><input hidden type="text" class="form-control " name="Empresa[]" readonly value="Doctor Pez"></td>';
         tr += '<td style="visibility:collapse; display:none;" class="Fecha"><input hidden type="text" class="form-control " name="FechaDeVenta[]" readonly value="<?php echo $ValorCaja["Fecha_Apertura"]; ?>"></td>';
         tr += '<td style="visibility:collapse; display:none;" class="FormaPago"><input hidden type="text" class="form-control forma-pago-input" id="FormaPagoCliente" name="FormaDePago[]" value="Efectivo"></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Descuentosugerido"><input hidden type="text" class="form-control descuento-aplicado" id="descuentoaplicado_' + articulo.id + '" name="DescuentoAplicado[]" readonly></td>';
         tr += '<td><div class="btn-container">' + btnEliminar + '</div><div class="input-container">' + inputId + inputCantidad + '</div><div class="btn-container"><a class="btn btn-info btn-sm" href="#" onclick="abrirSweetAlert(this); return false;"><i class="fas fa-percentage"></i></a></div></td>';
         tr += '</tr>';
 
