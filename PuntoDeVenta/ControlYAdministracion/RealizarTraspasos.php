9e13ec0b7710b745170d22c6bb5b4952ea872ebf
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2025-01-15 23:03:13 -0600

    Update RealizarTraspasos.php

--- PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
+++ PuntoDeVenta/ControlYAdministracion/RealizarTraspasos.php
@@ -957,96 +957,71 @@ $('#codigoEscaneado').autocomplete({
   // Variable para almacenar el total del IVA
   var totalIVA = 0;
   function agregarArticulo(articulo) {
-    const fechaApertura = document.getElementById('fecha-apertura').value; // Obtener la fecha seleccionada
-    if (!articulo || (!articulo.id && !articulo.descripcion)) {
-        mostrarMensaje('El artículo no es válido');
-        return;
-    } else if ($('#detIdModal' + articulo.id).length) {
-        mostrarMensaje('El artículo ya se encuentra incluido');
-        return;
+  if (!articulo || (!articulo.id && !articulo.descripcion)) {
+    mostrarMensaje('El artículo no es válido');
+    return;
+  }
+
+  let row = articulo.id
+    ? $('#tablaAgregarArticulos tbody').find('tr[data-id="' + articulo.id + '"]')
+    : null;
+
+  if (!row || !row.length) {
+    $('#tablaAgregarArticulos tbody tr').each(function () {
+      if ($(this).find('.descripcion-producto-input').val() === articulo.descripcion) {
+        row = $(this);
+        return false;
+      }
+    });
+  }
+
+  if (row && row.length) {
+    let cantidadActual = parseInt(row.find('.cantidad input').val());
+    let nuevaCantidad = cantidadActual + parseInt(articulo.cantidad);
+
+    if (nuevaCantidad < 0) {
+      mostrarMensaje('La cantidad no puede ser negativa');
+      return;
     }
 
-    var row = null;
-    if (articulo.id) {
-        row = $('#tablaAgregarArticulos tbody').find('tr[data-id="' + articulo.id + '"]');
-    } else {
-        // Buscar el primer artículo que coincida con la descripción si no hay un ID
-        $('#tablaAgregarArticulos tbody tr').each(function() {
-            if ($(this).find('.descripcion-producto-input').val() === articulo.descripcion) {
-                row = $(this);
-                return false; // Salir del bucle each cuando se encuentre el artículo
-            }
-        });
-    }
+    row.find('.cantidad input').val(nuevaCantidad);
+    mostrarToast('Cantidad actualizada para el producto: ' + articulo.descripcion);
+    actualizarImporte(row);
+    calcularIVA();
+    actualizarSuma();
+    mostrarTotalVenta();
+    mostrarSubTotal();
+    mostrarIvaTotal();
+  } else {
+    const btnEliminar = '<button type="button" class="btn btn-danger btn-sm" onclick="eliminarFila(this);"><i class="fas fa-minus-circle fa-xs"></i></button>';
 
-    if (row && row.length) {
-        var cantidadActual = parseInt(row.find('.cantidad input').val());
-        var nuevaCantidad = cantidadActual + parseInt(articulo.cantidad);
-        if (nuevaCantidad < 0) {
-            mostrarMensaje('La cantidad no puede ser negativa');
-            return;
-        }
-        row.find('.cantidad input').val(nuevaCantidad);
-        actualizarImporte(row);
-        calcularIVA();
-        actualizarSuma();
-        mostrarTotalVenta();
-        mostrarSubTotal();
-        mostrarIvaTotal();
-    } else {
-        var tr = '';
-        var btnEliminar = '<button type="button" class="btn btn-danger btn-sm" onclick="eliminarFila(this);"><i class="fas fa-minus-circle fa-xs"></i></button>';
+    const tr = `
+      <tr data-id="${articulo.id}">
+        <td class="codigo"><input class="form-control codigo-barras-input" readonly style="font-size: 0.75rem !important;" type="text" value="${articulo.codigo || ''}" name="CodBarras[]" /></td>
+        <td class="descripcion"><textarea class="form-control descripcion-producto-input" readonly style="font-size: 0.75rem !important;" name="NombreDelProducto[]">${articulo.descripcion}</textarea></td>
+        <td class="cantidad"><input class="form-control cantidad-vendida-input" style="font-size: 0.75rem !important;" type="number" name="Cantidad[]" value="${articulo.cantidad}" onchange="actualizarImporte($(this).parent().parent());" /></td>
+        <td class="preciofijo"><input class="form-control preciou-input" readonly style="font-size: 0.75rem !important;" type="number" value="${articulo.precio}" /></td>
+        <td style="display:none;" class="precio"><input hidden id="precio_${articulo.id}" class="form-control precio" style="font-size: 0.75rem !important;" type="number" name="Pc[]" value="${articulo.precio}" onchange="actualizarImporte($(this).parent().parent());" /></td>
+        <td><input id="importe_${articulo.id}" class="form-control importe" name="ImporteGenerado[]" style="font-size: 0.75rem !important;" type="number" readonly /></td>
+        <td style="visibility:collapse; display:none;" class="idbd"><input class="form-control" style="font-size: 0.75rem !important;" type="text" value="${articulo.id}" name="IdBasedatos[]" /></td>
+        <td><div class="btn-container">${btnEliminar}</div></td>
+      </tr>`;
 
-        var inputId = '<input type="hidden" name="detIdModal[' + articulo.id + ']" value="' + articulo.id + '" />';
-        var inputCantidad = '<input class="form-control" type="hidden" name="detCantidadModal[' + articulo.id + ']" value="' + articulo.cantidad + '" />';
+    $('#tablaAgregarArticulos tbody').append(tr);
 
-        tr += '<tr data-id="' + articulo.id + '">';
-        tr += '<td class="codigo"><input class="form-control codigo-barras-input" id="codBarrasInput" style="font-size: 0.75rem !important;" type="text" value="' + (articulo.codigo || '') + '" name="CodBarras[]" /></td>';
-        tr += '<td class="descripcion"><textarea class="form-control descripcion-producto-input" id="descripcionproducto" name="NombreDelProducto[]" style="font-size: 0.75rem !important;">' + articulo.descripcion + '</textarea></td>';
-        tr += '<td class="cantidad"><input class="form-control cantidad-vendida-input" style="font-size: 0.75rem !important;" type="number" name="Cantidad[]" value="' + articulo.cantidad + '" onchange="actualizarImporte($(this).parent().parent());" /></td>';
-        tr += '<td class="preciofijo"><input class="form-control preciou-input" style="font-size: 0.75rem !important;" type="number" value="' + articulo.precio + '" /></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="precio"><input hidden id="precio_' + articulo.id + '" class="form-control precio" style="font-size: 0.75rem !important;" type="number" name="Pc[]" value="' + articulo.precio + '" onchange="actualizarImporte($(this).parent().parent());" /></td>';
-        tr += '<td><input id="importe_' + articulo.id + '" class="form-control importe" name="ImporteGenerado[]" style="font-size: 0.75rem !important;" type="number" readonly /></td>';
-        tr += '<td style="visibility:collapse; display:none;"><input id="importe_siniva_' + articulo.id + '" class="form-control importe_siniva" type="number" readonly /></td>';
-        tr += '<td style="visibility:collapse; display:none;"><input id="valordelniva_' + articulo.id + '" class="form-control valordelniva" type="number" readonly /></td>';
-        tr += '<td style="visibility:collapse; display:none;"><input id="ieps_' + articulo.id + '" class="form-control ieps" type="number" readonly /></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="idbd"><input class="form-control" style="font-size: 0.75rem !important;" type="text" value="' + articulo.id + '" name="IdBasedatos[]" /></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="lote"><input class="form-control" style="font-size: 0.75rem !important;" type="text" value="' + articulo.lote + '" name="LoteDelProducto[]" /></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="claveess"><input class="form-control" style="font-size: 0.75rem !important;" type="text" value="' + articulo.clave + '" name="ClaveAdicional[]" /></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="tiposservicios"><input class="form-control" style="font-size: 0.75rem !important;" type="text" value="' + articulo.tipo + '" name="Tipo[]" /></td>';
+    const newRow = $('#tablaAgregarArticulos tbody tr:last-child');
+    actualizarImporte(newRow);
+    newRow.find('.tipoajuste-input').val(selectedAdjustment);
+    calcularIVA();
+    actualizarSuma();
+    mostrarTotalVenta();
+    mostrarSubTotal();
+    mostrarIvaTotal();
+  }
 
-            tr += '<td style="visibility:collapse; display:none;" class="NumeroTicket"><input type="text" class="form-control " hidden id="Folio_Ticket" name="Folio_Ticket[]" style="font-size: 0.75rem !important;" value="<?php echo $resultado_en_mayusculas; ?>" readonly></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Vendedor"><input hidden id="VendedorFarma" type="text" class="form-control " name="AgregadoPor[]" readonly value="<?php echo $row['Nombre_Apellidos'] ?>"></td>';
-        tr += '<td class="TipoMovimiento"><input  type="text" class="form-control " name="TipoDeMov[]" readonly value=""></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Sucursal"><input hidden type="text" class="form-control " name="Fk_sucursal[]" readonly value="<?php echo $row['Fk_Sucursal'] ?>"></td>';
-        tr += '<td  class="SucursalDestino"><input type="text" class="form-control tipoajuste-input " id="inputDestino"name="Fk_SucursalDestino[]" readonly ></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Sistema"><input hidden type="text" class="form-control " name="Sistema[]" readonly value="POSVENTAS"></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Liquidado"><input hidden type="text" class="form-control " name="Liquidado[]" readonly value="N/A"></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="N/A"><input hidden type="text" class="form-control " name="Estatus[]" readonly value="Generado"></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="Empresa"><input hidden type="text" class="form-control " name="ID_H_O_D[]" readonly value="Doctor Pez"></td>';
-        tr += '<td " class="Fecha"><input type="date" class="form-control " name="FechaVenta[]" id="fecha-apertura2-' + articulo.id + '" readonly value=""></td>';
-        tr += '<td style="visibility:collapse; display:none;" class="FormaPago"><input hidden type="text" class="form-control forma-pago-input" id="FormaPagoCliente" name="FormaDePago[]" value="Efectivo"></td>';
-        tr += '<td><div class="btn-container">' + btnEliminar + '</div><div class="input-container">' + inputId + inputCantidad + '</div><div class="btn-container"></td>';
-        tr += '</tr>';
-
-        $('#tablaAgregarArticulos tbody').append(tr);
-        document.getElementById('fecha-apertura2-' + articulo.id).value = fechaApertura;
-        actualizarImporte($('#tablaAgregarArticulos tbody tr:last-child'));
-        newRow.find('.tipoajuste-input').val(selectedAdjustment);
-        calcularIVA();
-        actualizarSuma();
-        mostrarTotalVenta();
-        mostrarSubTotal();
-        mostrarIvaTotal();
-        
-    }
-
-    $('#codigoEscaneado').val('');
-    $('#codigoEscaneado').focus();
+  limpiarCampo();
+  $('#codigoEscaneado').focus();
 }
-
-
-  
 
 
 
