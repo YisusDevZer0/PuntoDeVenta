23599607223bd77344195b72aea7d3e73a2f932d
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2025-01-21 22:09:01 -0600

    Update NuevoTipoUsuario.php

--- PuntoDeVenta/ControlYAdministracion/Modales/NuevoTipoUsuario.php
+++ PuntoDeVenta/ControlYAdministracion/Modales/NuevoTipoUsuario.php
@@ -2,9 +2,9 @@
 <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Agregar Nuevo Tipo De Usuario</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-header" style=" background-color: #ef7980 !important;">
+    <h5 class="modal-title" id="exampleModalLabel" style="color:white" >Agregar Nuevo Personal</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <!-- Formulario con el estilo proporcionado -->
