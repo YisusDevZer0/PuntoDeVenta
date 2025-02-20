0d9896bb64a986885169e7026640b27c8e040404
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2025-02-19 22:24:27 -0600

    Update index.php

--- PuntoDeVenta/ControlYAdministracion/index.php
+++ PuntoDeVenta/ControlYAdministracion/index.php
@@ -85,7 +85,7 @@ include_once "Controladores/ConsultaDashboard.php";
    
    <?php
    include "header.php";?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+
 
 <body>
 <script>
