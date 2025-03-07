f892467de62bbd80371ee62cb5ca75aea005b6ca
YisusDevZer0 <155038647+YisusDevZer0@users.noreply.github.com>
2025-03-06 22:28:49 -0600

    Update sw.js

--- PuntoDeVenta/PuntoDeVentaFarmacias/sw.js
+++ PuntoDeVenta/PuntoDeVentaFarmacias/sw.js
@@ -1,19 +1,31 @@
 const CACHE_NAME = 'punto-de-venta-cache-v1';
 const urlsToCache = [
   '/PuntoDeVenta/PuntoDeVentaFarmacias/RealizarVentas.php',
-  
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Intentando cachear recursos:', urlsToCache);
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error('Error al cachear recursos:', error);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          console.log('Recurso encontrado en caché:', event.request.url);
+          return response;
+        }
+        console.log('Recurso no encontrado en caché, solicitando al servidor:', event.request.url);
+        return fetch(event.request);
+      })
   );
 });
