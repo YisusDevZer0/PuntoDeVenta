4e1a6c056e5989141f18a1a361b3d3a956e1db87
DevZero <jesusemutul@gmail.com>
2025-03-29 11:03:45 -0600

    composer fix

--- PuntoDeVenta/ControlYAdministracion/ActualizacionMasivaMaxMin.php
+++ PuntoDeVenta/ControlYAdministracion/ActualizacionMasivaMaxMin.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include('dbconect.php');
-require 'vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
