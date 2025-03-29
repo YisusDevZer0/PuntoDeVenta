c05e92f8ca85dfb05e7c0401c955ddd80768b059
DevZero <jesusemutul@gmail.com>
2025-03-29 11:38:53 -0600

    fix

--- PuntoDeVenta/ControlYAdministracion/ActualizacionMasivaMaxMin.php
+++ PuntoDeVenta/ControlYAdministracion/ActualizacionMasivaMaxMin.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include('dbconect.php');
-require 'vendor/autoload.php';
+
 // Verificar si el archivo autoload.php existe
 $autoloadPath = __DIR__ . '/../../vendor/autoload.php';
 if (!file_exists($autoloadPath)) {
@@ -12,6 +12,7 @@ if (!file_exists($autoloadPath)) {
 }
 
 require $autoloadPath;
+
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 if (isset($_POST["preview"])) {
