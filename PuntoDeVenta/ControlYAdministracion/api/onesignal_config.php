e6cb5c4fa8f53e3bd1ea52a347ffdad4cf1c188d
DevZero <jesusemutul@gmail.com>
2025-05-21 22:52:47 -0600

    fix api

--- PuntoDeVenta/ControlYAdministracion/api/onesignal_config.php
+++ PuntoDeVenta/ControlYAdministracion/api/onesignal_config.php
@@ -1,8 +1,8 @@
 <?php
 // Configuración de OneSignal
 return [
-    'app_id' => 'os_v2_app_g6av4dtklvawjf736pnnyk6lfghofcvkoede5rn3rg7kwgyodhsdsw2nwqzbbvwvaav3xpgfjkvv2bx6ot5u7zrfilivicsslda77ta',
-    'rest_api_key' => '37815e0e-6a5d-4164-97fb-f3dadc2bcb29',
+    'app_id' => '37815e0e-6a5d-4164-97fb-f3dadc2bcb29',
+    'rest_api_key' => 'hofcvkoede5rn3rg7kwgyodhs',
     'safari_web_id' => '', // Opcional, para soporte de Safari
     'default_icon' => 'https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/assets/img/logo.png',
     'default_url' => 'https://doctorpez.mx/PuntoDeVenta/ControlYAdministracion/'
