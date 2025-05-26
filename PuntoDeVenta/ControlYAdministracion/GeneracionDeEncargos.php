837fad50e5093dbb86a33a82b2483f9426dc022d
DevZero <jesusemutul@gmail.com>
2025-05-25 21:05:51 -0600

    xdd

--- PuntoDeVenta/ControlYAdministracion/GeneracionDeEncargos.php
+++ PuntoDeVenta/ControlYAdministracion/GeneracionDeEncargos.php
@@ -22,10 +22,7 @@ $ValorFondoCaja = mysqli_fetch_assoc($resultset);
     </div>
     <?php
     include "header.php"; ?>
-<!-- Agregar las dependencias de jQuery UI -->
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 <body>
 
