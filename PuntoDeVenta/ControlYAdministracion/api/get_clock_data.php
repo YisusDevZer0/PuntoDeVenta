9bd914d943b62915e6518b4cddae9db19c527d72
DevZero <jesusemutul@gmail.com>
2025-05-26 22:21:05 -0600

    xd

--- PuntoDeVenta/ControlYAdministracion/api/get_clock_data.php
+++ PuntoDeVenta/ControlYAdministracion/api/get_clock_data.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Incluir conexión a la base de datos
-include "../db_connection_Huellas";
+include "../../db_connection_Huellas.php";
 session_start();
 
 header('Content-Type: application/json');
