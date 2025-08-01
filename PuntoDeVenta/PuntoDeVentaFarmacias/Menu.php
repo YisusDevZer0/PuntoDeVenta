e4ac0f2aee1c28dbd96e1dd6e2ed4286e16e7446
DevZero <jesusemutul@gmail.com>
2025-08-01 00:32:58 -0600

    Mejorar la accesibilidad del menú de navegación en la aplicación de farmacias al agregar el atributo 'aria-expanded' en los elementos dropdown. Además, se elimina el archivo de prueba 'test_menu.html' que ya no es necesario para el funcionamiento del sistema.

--- PuntoDeVenta/PuntoDeVentaFarmacias/Menu.php
+++ PuntoDeVenta/PuntoDeVentaFarmacias/Menu.php
@@ -39,7 +39,7 @@ if (!isset($showDashboard)) {
             <a href="index" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Inicio</a>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-money-bill-transfer"></i>Punto de venta</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-money-bill-transfer"></i>Punto de venta</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="AperturarCajaV2" class="dropdown-item"><i class="fa-solid fa-cash-register"></i> Apertura de caja</a>
                     <a href="RealizarVentas" class="dropdown-item"><i class="fa-solid fa-hand-holding-dollar"></i> Realizar Ventas</a>
@@ -48,7 +48,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-house-medical"></i>Farmacia</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-house-medical"></i>Farmacia</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="RegistrosdeEnergia" class="dropdown-item"><i class="fa-solid fa-lightbulb"></i>Registro de energia</a>
                     <a href="BitacoraLimpieza" class="dropdown-item"><i class="fa-solid fa-broom"></i>Bitacora de limpieza</a>
@@ -60,7 +60,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-calendar"></i>Agendas</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-calendar"></i>Agendas</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="AgendaRevaloraciones" class="dropdown-item"><i class="fa-solid fa-person-walking-arrow-loop-left"></i>Revaloraciones</a>
                     <a href="AgendaEspecialistas" class="dropdown-item"><i class="fa-solid fa-hospital-user"></i>Especialistas</a>
@@ -69,7 +69,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-cash-register"></i>Cajas</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-cash-register"></i>Cajas</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="CortesDeCaja" class="dropdown-item"><i class="fa-solid fa-file-invoice-dollar"></i>Cortes de caja</a>
                     <a href="RegistroDeGastos" class="dropdown-item"><i class="fa-solid fa-comment-dollar"></i>Registro de gastos</a>
@@ -77,7 +77,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-users"></i>Clientes</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-users"></i>Clientes</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="DatosDeClientes" class="dropdown-item"><i class="fa-solid fa-people-group"></i>Lista de clientes</a>
                     <a href="Creditos" class="dropdown-item"><i class="fa-regular fa-credit-card"></i>Créditos</a>
@@ -86,7 +86,7 @@ if (!isset($showDashboard)) {
             </div>
 
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-receipt"></i>Tickets</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-receipt"></i>Tickets</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="Tickets" class="dropdown-item"><i class="fa-solid fa-ticket-simple"></i>Desglose de tickets</a>
                     <a href="AbonosEnCreditos" class="dropdown-item"><i class="fa-solid fa-money-bills"></i>Tickets de crédito</a>
@@ -95,7 +95,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-chart-line"></i>Ventas</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-chart-line"></i>Ventas</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="VentasDelDia" class="dropdown-item"><i class="fa-solid fa-coins"></i>Ventas del día</a>
                     <a href="VentasAcredito" class="dropdown-item"><i class="fa-solid fa-credit-card"></i>Ventas a crédito</a>
@@ -103,7 +103,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-boxes-stacked"></i>Almacén</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-boxes-stacked"></i>Almacén</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="StockSucursal" class="dropdown-item"><i class="fa-solid fa-dolly"></i>Stock</a>
                     <a href="IngresosDeMedicamentos" class="dropdown-item"><i class="fa-solid fa-box"></i>Stock de insumos</a>
@@ -115,7 +115,7 @@ if (!isset($showDashboard)) {
             </div>
             
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-arrow-right-arrow-left"></i>Traspasos</a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-arrow-right-arrow-left"></i>Traspasos</a>
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="RealizarTraspasos" class="dropdown-item"><i class="fa-solid fa-exchange-alt"></i>Realizar Traspasos</a>
                     <a href="ListadoDeTraspasos" class="dropdown-item"><i class="fa-solid fa-list-check"></i>Listado de traspasos</a>
